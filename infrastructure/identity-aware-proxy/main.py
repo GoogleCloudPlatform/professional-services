92529c8ec87102ea4b157bb11659e4eeeb2d7e12
Daniel De Leo <ddeleo@google.com>
2017-08-22 15:12:32 -0400

    cleanup main.py for calling prog auth

--- infrastructure/identity-aware-proxy/main.py
+++ infrastructure/identity-aware-proxy/main.py
@@ -5,6 +5,3 @@ if __name__ == "__main__":
 	# sys.argv[1] = URL
 	# sys.argv[2] = IAP Client Id
 	make_iap_request(sys.argv[1],sys.argv[2])
-	# make_iap_request(
-	# 	"https://gappslabs.co",
-	# 	"209762747271-knpi7rgeu1q1a8c6degjk3f4m9bsmcco.apps.googleusercontent.com")
