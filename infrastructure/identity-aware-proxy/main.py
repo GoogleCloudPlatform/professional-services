0a7997ca7123d5ee6146ab4c6cfe8966a0831bf4
Daniel De Leo <ddeleo@google.com>
2017-08-23 10:10:12 -0400

    print output of main.py

--- infrastructure/identity-aware-proxy/main.py
+++ infrastructure/identity-aware-proxy/main.py
@@ -4,4 +4,4 @@ from make_iap_request import *
 if __name__ == "__main__":
 	# sys.argv[1] = URL
 	# sys.argv[2] = IAP Client Id
-	make_iap_request(sys.argv[1],sys.argv[2])
+	print make_iap_request(sys.argv[1],sys.argv[2])
