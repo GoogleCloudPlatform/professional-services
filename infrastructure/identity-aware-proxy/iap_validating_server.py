38d03b747aeea20086b88e1db6e1dc20d8343e7a
Daniel De Leo <ddeleo@google.com>
2017-08-28 18:30:00 -0400

    updating with proposed changes from pull reqeust

--- infrastructure/identity-aware-proxy/iap_validating_server.py
+++ infrastructure/identity-aware-proxy/iap_validating_server.py
@@ -11,48 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-# validate_jwt github link: 
+
+"""Example of verifying IAP signed headers in web requests."""
+
+from BaseHTTPServer import BaseHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
+# validate_jwt github link:
 #    https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/iap/validate_jwt.py
 import validate_jwt
 
 
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        """Intercepts all GET requests and validates 
-        the IAP JWT that is present in the header.
-        For this example, all requests must have the
-        load balancer's backend service id and the 
-        project number present in the url. 
 
-        Example request: 
-        https:testdomain.com/projectNumber/backendServiceId
+  def do_GET(self):
+    """Intercepts all GET requests and validates
+    the IAP JWT that is present in the header.
+    For this example, all requests must have the
+    load balancer's backend service id and the
+    project number present in the url.
 
-        """
-        print self.headers
-        backendServiceId = self.path.split("/")[-1]
-        projectNumber = self.path.split("/")[-2]
-        identity = validate_jwt.validate_iap_jwt_from_compute_engine(
-                    self.headers.get("X-Goog-IAP-JWT-Assertion"),
-                    projectNumber,
-                    backendServiceId)          
-        if not identity[1]:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write("IAP Validation Failed")
-        else:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write("Hello " + identity[1] + "!")
-        return
+    Example request:
+    https:testdomain.com/projectNumber/backendServiceId
+    """
+    print self.headers
+    project_number = self.path.split("/")[-2]
+    backend_service_id = self.path.split("/")[-1]
+    identity = validate_jwt.validate_iap_jwt_from_compute_engine(
+        self.headers.get("X-Goog-IAP-JWT-Assertion"),
+        project_number,
+        backend_service_id)
+    if not identity[1]:
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+      self.wfile.write("IAP Validation Failed")
+    else:
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+      self.wfile.write("Hello " + identity[1] + "!")
+    return
 
 
 def main():
-    port = 80
-    print 'Listening on localhost:%s' % port
-    server = HTTPServer(('', port), RequestHandler)
-    server.serve_forever()
+  port = 80
+  print "Listening on localhost:%s" % port
+  server = HTTPServer(("", port), RequestHandler)
+  server.serve_forever()
 if __name__ == "__main__":
-    main()
+  main()
