c626c87864262f5fe5caa739351be0a894247ee3
Daniel De Leo <ddeleo@google.com>
2017-08-28 18:42:35 -0400

    updating with proposed changes from pull reqeust

--- infrastructure/identity-aware-proxy/main.py
+++ infrastructure/identity-aware-proxy/main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example use make_iap_request.py sample."""
+"""Example making GET request to IAP resource."""
 
 import argparse
 import make_iap_request as iap
