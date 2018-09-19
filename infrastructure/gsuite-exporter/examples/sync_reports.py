292029c507eb9abfe11dc0339bec4fce3f631770
Olivier Cervello <ocervello@google.com>
2018-09-19 18:35:53 -0500

    Update sync_reports.py

--- infrastructure/gsuite-exporter/examples/sync_reports.py
+++ infrastructure/gsuite-exporter/examples/sync_reports.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     ]
     for app in APPLICATIONS:
         sync_all(
-            os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
             "<user>@<domain>.rocks",
             app,
             "<project-id>",
