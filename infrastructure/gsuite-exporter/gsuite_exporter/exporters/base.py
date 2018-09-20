10d52859a8c4901bf20a9a28de97ac8c83ba3a70
Olivier Cervello <ocervello@google.com>
2018-09-19 22:28:07 -0500

    Update base.py

--- infrastructure/gsuite-exporter/gsuite_exporter/exporters/base.py
+++ infrastructure/gsuite-exporter/gsuite_exporter/exporters/base.py
@@ -13,15 +13,14 @@
 # limitations under the License.
 
 class BaseExporter(object):
-    """Convert Admin SDK logs to logging entries and sends them to an export
+    """Convert Admin SDK logs to a compatible format and sends them to an export
     destination.
 
     Args:
         api (`googleapiclient.discovery.Resource`): The Admin SDK API to fetch
             records from.
         version (str): The Admin SDK API version.
-        credentials_path (str): The path to the GSuite Admin credentials.
-        scopes (list): A list of scopes to grant the API requests.
+        credentials_path (str, optional): The path to the GSuite Admin credentials.
     """
 
     def __init__(self,
