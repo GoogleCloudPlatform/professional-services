61e72858d2367568c6f889d186f29a6dde8df165
Olivier Cervello <ocervello@google.com>
2018-09-19 21:36:01 -0500

    Update cli.py

--- infrastructure/gsuite-exporter/gsuite_exporter/cli.py
+++ infrastructure/gsuite-exporter/gsuite_exporter/cli.py
@@ -70,7 +70,7 @@ def sync_all(
 def main():
     parser = argparse.ArgumentParser(description='Add some integers.')
     parser.add_argument('--admin-user', type=str, help='The GSuite Admin user email.', required=True)
-    parser.add_argument('--application', type=str, help='The GSuite Admin Application', required=True)
+    parser.add_argument('--application', type=str, help='The GSuite Admin Application.', required=True)
     parser.add_argument('--project-id', type=str, help='The project id to export GSuite data to.', required=True)
     parser.add_argument('--exporter', type=str, help='The exporter class to use.', default='stackdriver_exporter.StackdriverExporter', required=False)
     parser.add_argument('--credentials-path', type=str, help='GSuite Admin credentials file.', default=None, required=False)
