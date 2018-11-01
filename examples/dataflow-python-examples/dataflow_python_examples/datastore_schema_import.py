0f6299c2e60f5f9511abce5110cf3b440e5f743c
Lorenzo Caggioni <lcaggioni@google.com>
2018-11-01 08:38:12 +0100

    fixes

--- examples/dataflow-python-examples/dataflow_python_examples/datastore_schema_import.py
+++ examples/dataflow-python-examples/dataflow_python_examples/datastore_schema_import.py
@@ -1,16 +1,17 @@
-# Copyright 2018 Google LLC. All rights reserved. Licensed under the Apache License, 
-# Version 2.0 (the "License"); you may not use this file except in compliance with 
-# the License. You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed 
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-# specific language governing permissions and limitations under the License.
-#
-# Any software provided by Google hereunder is distributed "AS IS", WITHOUT 
-# WARRANTIES OR CONDITIONS OF ANY KIND, and is not intended for production use.
+# Copyright 2018 Google Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Script to import file schema from CSV and save it into Google Cloud DataStore
 To run this script, you will need Python packages listed in REQUIREMENTS.txt.
 You can easily install them with virtualenv and pip by running these commands:
@@ -24,12 +25,14 @@ Example to run the script:
 python dataflow_import.py \
 --input-folder=$INPUT_FOLDER
 """
+
 import argparse
 import collections
 import csv
 import json
 import os
 from google.cloud import datastore
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -57,5 +60,6 @@ def main():
         client.put(entry)
         print ('Created/Updated entry for table %s.' % table)
     print ('Done.')
+    
 if __name__ == '__main__':
     main()
