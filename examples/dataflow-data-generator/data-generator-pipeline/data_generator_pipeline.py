105861c5167a2486591fb627d620ba00149d4126
Jacob Ferriero <jferriero@google.com>
2018-11-20 14:59:17 -0500

    added step for fixing avro datetimes
    fix typo
    
    fix typo

--- examples/dataflow-data-generator/data-generator-pipeline/data_generator_pipeline.py
+++ examples/dataflow-data-generator/data-generator-pipeline/data_generator_pipeline.py
@@ -33,7 +33,7 @@ from data_generator.DataGenerator import DataGenerator, FakeRowGen, \
 import avro.schema
 
 from data_generator.CsvUtil import dict_to_csv
-
+from data_generator.AvroUtil import fix_record_for_avro
 
 def run(argv=None):
     """
@@ -101,9 +101,12 @@ def run(argv=None):
         )
 
     if data_args.avro_schema_file:
-        avsc = avro.schema.parse(open(data_args.avro_schema_file,'rb').read())
+        avsc = avro.schema.parse(open(data_args.avro_schema_file, 'rb').read())
 
         (rows
+            # Need to convert time stamps from strings to timestamp-micros
+            | 'Fix date and time Types for Avro.' >> beam.FlatMap(lambda row: 
+                fix_record_for_avro(row, schema=data_gen.schema))
             | 'Write to Avro.' >> beam.io.avroio.WriteToAvro(
                     file_path_prefix=data_args.output_prefix,
                     codec='null',
