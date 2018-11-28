e185d26320e80438fe46d85f20c566d88fa208e4
kardiff18 <22534191+kardiff18@users.noreply.github.com>
2018-11-28 11:33:07 -0500

    Update config.py

--- tools/kunskap/config.py
+++ tools/kunskap/config.py
@@ -1,10 +1,9 @@
 # EDIT THESE WITH YOUR OWN DATASET/TABLES
-project_id = 'project_id'
+billing_project_id = 'project_id'
 billing_dataset_id = 'billing_dataset'
 billing_table_name = 'billing_data'
 output_dataset_id = 'output_dataset'
 output_table_name = 'transformed_table'
-bucket_id = 'bucket_id'
+
 # You can leave this unless you renamed the file yourself.
 sql_file_path = 'cud_sud_attribution_parameterized.sql'
-
