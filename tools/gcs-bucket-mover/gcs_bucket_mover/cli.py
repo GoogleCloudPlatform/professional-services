4f3e262eec93b9f77d31412bace47ffdfd90422f
Matt Geneau <mgeneau@google.com>
2019-01-14 17:33:13 -0800

    Configurable lock, logging to Stackdriver and packaging with Pex.

--- tools/gcs-bucket-mover/gcs_bucket_mover/cli.py
+++ tools/gcs-bucket-mover/gcs_bucket_mover/cli.py
@@ -17,38 +17,53 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import argparse
 import textwrap
+
+import configargparse
+import yaml
 
 from gcs_bucket_mover import bucket_mover_service
 from gcs_bucket_mover import bucket_mover_tester
 
 
 def get_config():
-    """Parses command line arguments.
+    """Parses command line arguments and the config file.
 
-    Args:
-        None
+    Order of precedence for config values is: command line > config file values > defaults
 
     Returns:
         A "Namespace" object. See argparse.ArgumentParser.parse_args() for more details.
     """
 
-    parser = argparse.ArgumentParser(
+    parser = configargparse.ArgumentParser(
         description=
         'Moves a GCS bucket from one project to another, along with all objects and optionally'
         ' copying all other bucket settings.',
-        formatter_class=argparse.RawTextHelpFormatter)
+        config_file_parser_class=configargparse.YAMLConfigFileParser,
+        formatter_class=configargparse.RawTextHelpFormatter)
     parser.add_argument(
-        'bucket_name', type=str, help='The name of the bucket to be moved.')
+        '--config',
+        is_config_file=True,
+        help='The path to the local config file')
+
+    parser.add_argument(
+        'bucket_name', help='The name of the bucket to be moved.')
     parser.add_argument(
         'source_project',
-        type=str,
         help='The project id that the bucket is currently in.')
     parser.add_argument(
         'target_project',
-        type=str,
         help='The project id that the bucket will be moved to.')
+    parser.add_argument(
+        '--gcp_source_project_service_account_key',
+        help=
+        'The location on disk for service account key json file from the source project'
+    )
+    parser.add_argument(
+        '--gcp_target_project_service_account_key',
+        help=
+        'The location on disk for service account key json file from the target project'
+    )
     parser.add_argument(
         '--test',
         action='store_true',
@@ -57,19 +72,27 @@ def get_config():
         buckets can be moved between the two projects, using a randomly generated bucket.
         A fake bucket name will still need to be specified.'''))
     parser.add_argument(
+        '--useBucketLock',
+        action='store_true',
+        help=textwrap.dedent('''\
+        Enabling this option will mean that before the mover makes any changes, it will look for the lock file specified
+        in LOCK_FILE_NAME (in config.sh). If it exists in the source bucket, the mover will exit without any operations.
+        If it does not exist, the permissions on the source project will be updated so that nobody is allowed to write
+        to it, then the mover will move the buckets before finally restoring write access on the new bucket in the
+        target project.'''))
+    parser.add_argument(
+        '--lock_file_name', help='The name of the lock file in the bucket')
+    parser.add_argument(
         '--tempBucketName',
-        type=str,
-        help='The termporary bucket name to use in the target project.')
+        help='The temporary bucket name to use in the target project.')
     parser.add_argument(
         '--location',
-        type=str,
         help='Specify a different location for the target bucket.')
     parser.add_argument(
         '--storageClass',
-        type=str,
         choices=[
-            'MULTI_REGIONAL', 'NAM4', 'REGIONAL', 'STANDARD', 'NEARLINE',
-            'COLDLINE', 'DURABLE_REDUCED_AVAILABILITY'
+            'MULTI_REGIONAL', 'REGIONAL', 'STANDARD', 'NEARLINE', 'COLDLINE',
+            'DURABLE_REDUCED_AVAILABILITY'
         ],
         help='Specify a different storage class for the target bucket.')
     parser.add_argument(
@@ -125,7 +148,47 @@ def get_config():
         action='store_true',
         help='Don\'t copy the Versioning setting from the source bucket.')
 
+    # Variables set in the config file for running different bucket tests with the --test option
+    parser.add_argument(
+        '--test_bucket_location',
+        help='The location to create the test bucket in')
+    parser.add_argument(
+        '--test_default_kms_key_name',
+        help='A custom KSM key to assign to the test bucket')
+    parser.add_argument(
+        '--test_email_for_iam',
+        help='An IAM email to use for testing permissions on the test bucket')
+    parser.add_argument(
+        '--test_logging_bucket',
+        help='An existing bucket to set up logging on the test bucket')
+    parser.add_argument(
+        '--test_logging_prefix',
+        help='A prefix to use for the logging on the test bucket')
+    parser.add_argument(
+        '--test_storage_class',
+        help='The storage class to use for the test bucket')
+    parser.add_argument(
+        '--test_topic_name',
+        help='A topic name to set up a notification for on the test bucket')
+
     return parser.parse_args()
+
+
+def _parse_yaml_file(path):
+    """Load and parse local YAML file
+
+    Args:
+        path: either a local file system path or a GCS path
+
+    Returns:
+        a Python object representing the parsed YAML data
+    """
+
+    with open(path, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as ex:
+            print(ex)
 
 
 def main():
