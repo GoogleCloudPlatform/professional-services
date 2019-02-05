39cb9a9778af83753f89864b43a261c5e4b1b40b
Kaitlin <kardiff@google.com>
2019-02-05 11:31:00 -0500

    updated based on review changes

--- tools/kunskap/config.py
+++ tools/kunskap/config.py
@@ -1,4 +1,3 @@
-
 # EDIT THESE WITH YOUR OWN DATASET/TABLES
 billing_project_id = 'project_id'
 billing_dataset_id = 'billing_dataset'
@@ -11,12 +10,19 @@ sql_file_path = 'cud_sud_attribution_query.sql'
 
 # There are two slightly different allocation methods that affect how the Commitment charge is allocated:
 
-# Method 1: Utilized commitment charges are allocated to cost buckets proportionally to buckets share of 
-# total eligible VM usage during the time increment (P_usage_percentage).
-# any untilized commitment cost remains unallocated (BA_unutilized_commitment_cost).
+# There are two slightly different allocation methods that affect how the
+# Commitment charge is allocated:
 
-#Method 2: All commitment charges are allocated to buckets (P_method_2_CUD_commitment_cost) proportionally 
-# to the buckets share of total eligible VM usage during the time increment (P_usage_percentage). All 
-# commitment cost is allocated into the buckets proportionally to the CUD credits that they consumed, even 
-# if the commitment is not fully utilized.
-allocation_method = 'P_method_2_commitment_cost'
+# Method 1: Only UTILIZED commitment charges are allocated to projects.
+# (P_method_1_CUD_commitment_cost): Utilized CUD commitment charges are
+# proportionally allocated to each project based on its share of total eligible
+# VM usage during the time increment (P_usage_percentage). Any unutilized
+# commitment cost remains unallocated (BA_unutilized_commitment_cost) and is
+# allocated to the shell project.
+
+# Method 2: ALL commitment charges are allocated to projects (regardless of utilization).
+# (P_method_2_CUD_commitment_cost): All CUD commitment charges are
+# proportionally allocated to each project based on its share of total eligible
+# VM usage during the time increment (P_usage_percentage). All commitment cost
+# is allocated into the projects proportionally based on the CUD credits that
+# they consumed, even if the commitment is not fully utilized.
