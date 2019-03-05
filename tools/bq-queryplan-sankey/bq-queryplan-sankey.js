59a0550613b2dee88978d1d99a7385cc4ccafb12
Srinivas Sudhindra <ssudhindra@google.com>
2019-03-05 13:44:42 +0530

    Comments with http://usejsdoc.org/

--- tools/bq-queryplan-sankey/bq-queryplan-sankey.js
+++ tools/bq-queryplan-sankey/bq-queryplan-sankey.js
@@ -1,28 +1,38 @@
-// Copyright 2019 Google LLC
+/**
+ * Copyright 2019 Google LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     https://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// This file prepares the BigQuery query plan JSON data 
-// into a format acceptable by D3JS' Sankey library
-
+/**
+ * This file prepares the BigQuery query plan JSON data 
+ * into a format acceptable by D3JS' Sankey library
+ */
 var bqsankey = (function () {
-  // Check if stage is for repartitioning
+
+  /** 
+   * Check if stage is for repartitioning
+   * @param {string} stageName - Name of the stage
+   */
   function isRepartitionStage(stageName) {
     return stageName.match(/repartition/gi);
   };
 
-  // Get tables that input data into a stage
-  // Idea is to mimic tables as a stage in a query plan
+  /**
+   * Given a query plan and its stages, get list of tables referenced in the steps
+   * @param {Object[]} refTables - Array of referenced tables in the query
+   * @param {Object[]} queryPlan - BigQuery's query plan JSON
+   */
   function getInputTables(refTables, queryPlan) {
     // Input stage is any stage with direct input from a table
     var substepLines = queryPlan.steps.filter(function(step) { 
@@ -42,21 +52,35 @@ var bqsankey = (function () {
     });
   };
 
-  // Get stage based on stage id
+  /**
+   * Get stage based on stage id
+   * @param {Object[]} stages - Array of stage objects
+   * @param {string} stageid - stage id to find
+   */
   function getStage(stages, stageid) {
     return stages.find(function(stage) {
       return stage.id == stageid;
     });
   };
 
-  // Utility function to check empty array
+  // 
+  /**
+   * Utility function to check empty array
+   * @param {Object[]} arr - Array to check
+   */
   function arrayOrEmpty(arr) {
-    if (arr) return arr;
-    else return [];
+    if (arr) {
+      return arr;
+    } else {
+      return [];
+    }
   };
 
-  // Get unique stages from a list with duplicates 
-  // https://codeburst.io/javascript-array-distinct-5edc93501dc4
+  /**
+   * Get unique stages from a list with duplicates
+   * from https://codeburst.io/javascript-array-distinct-5edc93501dc4
+   * @param {Object[]} stages - Array of stage objects
+   */
   function unique(stages) {
     const result = [];
     if (!stages || stages.length == 0) return result;
@@ -75,9 +99,11 @@ var bqsankey = (function () {
     return result;
   };
 
-  // Parse query plan and get the stages. 
-  // Also add stages to correspond to tables, 
-  // to show data coming into query plan stages
+  /**
+   * Parse query plan to get relevant stage information for use in Sankey.
+   * Also add a layer of stages to show tables from which data is read from.
+   * @param {Object} bqjson - BigQuery query plan JSON
+   */
   function extractStages(bqjson) {
     var stages = bqjson.statistics.query.queryPlan.map(function(qp) {
       // Only pick properties needed
@@ -114,10 +140,12 @@ var bqsankey = (function () {
     return stages.concat(unique(tableStages));
   };
 
-  // Transform a stage to node format, expected by d3js Sankey library
-  // Ignore repartitioning stages as they represent 
-  // duplicate data points for a stage
-  // For Sankey to be correct, we need unique data points for each node and link
+  /**
+   * Create node objects needed by Sankey from stages
+   * Ignore repartitioning stages as they represent duplicate data points for a stage
+   * For a correct Sankey diagram, we need unique data points for each node and link
+   * @param {Object[]} stages - List of stage objects that absracts query plan information needed for Sankey
+   */
   function transformToNodes(stages) {
     return stages.filter(function(stage) {
       return !isRepartitionStage(stage.name);
@@ -126,7 +154,11 @@ var bqsankey = (function () {
     });
   };
 
-  // Transform stage to link format, expected by d3js Sankey library
+  /**
+   * Create link objects needed by Sankey from stages
+   * @param {Object[]} stages - List of stage objects that absracts query plan information needed for Sankey
+   * @param {Object[]} nodes - List of Sankey's node objects
+   */
   function transformToLinks(stages, nodes) {
     return stages.flatMap(function(stage) {
       return stage.inputStages.map(function(stageid) {
@@ -151,8 +183,11 @@ var bqsankey = (function () {
     });
   };
 
-  // Main function to be called to transform a query plan in JSONformat
-  // to nodes and links format expected by d3js' Sankey library
+  /**
+   * Main public function to be called to transform a query plan in JSONformat
+   * to nodes and links format expected by d3js' Sankey library
+   * @param {Object} bqjson - BigQuery query plan JSON
+   */
   function qp2sankey(bqjson) {
     var stages = extractStages(bqjson);
     var nodes = transformToNodes(stages);
@@ -160,6 +195,7 @@ var bqsankey = (function () {
     return { nodes: nodes, links: links };
   }
 
+  // public interface
   return { qp2sankey: qp2sankey };
 
 })();
