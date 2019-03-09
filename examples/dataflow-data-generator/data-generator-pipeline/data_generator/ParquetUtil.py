163e010b4f537d0ac207b2cb07a459d4172e033d
Anna Rudy <annarudy@google.com>
2019-03-08 19:16:46 -0600

    changing ParquetUtil.py to match parquetio_addition

--- examples/dataflow-data-generator/data-generator-pipeline/data_generator/ParquetUtil.py
+++ examples/dataflow-data-generator/data-generator-pipeline/data_generator/ParquetUtil.py
@@ -1,7 +1,9 @@
 import pyarrow as pa
+import logging
+import json
 
 
-def get_pyarrow_translated_schema(json_schema):
+def get_pyarrow_translated_schema(string_schema):
     type_conversions = {
         'STRING': pa.string(),
         'NUMERIC': pa.int64(),
@@ -10,19 +12,30 @@ def get_pyarrow_translated_schema(json_schema):
         'FLOAT': pa.float64(),
         'NUMERIC': pa.int64(),
         'BOOLEAN': pa.bool_(),
-        'TIMESTAMP': pa.timestamp(),
+        'TIMESTAMP': pa.timestamp('ms'),
         'DATE': pa.date32(),
-        'TIME': pa.time64(),
+        'TIME': pa.time64('us'),
         'DATETIME': pa.date64(),
         'GEOGRAPHY': None,
         'RECORD': None
     }
     pa_schema_list = []
-    for json_field in json_schema:
-        pa_field = pa.field(
-            json_field.name,
-            type_conversions[json_field.field_type]
-        )
-        pa_schema_list.append(pa_field)
+    for field in string_schema:
+        field_type = field['type']
+        field_name = field['name']
+        converted_field_type = type_conversions[field_type]
+        if converted_field_type is None:
+            logging.error('Error: json schema included a {0:s} field. BYTE,  '
+                          'GEOGRAPHY, and RECORD types cannot be used when '
+                          'outputting to parquet.'.format(field_type))
+            raise ValueError('Error: json schema included a {0:s} field. BYTE, '
+                             'GEOGRAPHY, and RECORD types cannot be used when '
+                             'outputting to parquet.'.format(field_type))
+        else:
+            pa_field = pa.field(
+                field_name,
+                converted_field_type
+            )
+            pa_schema_list.append(pa_field)
 
     return pa.schema(pa_schema_list)
