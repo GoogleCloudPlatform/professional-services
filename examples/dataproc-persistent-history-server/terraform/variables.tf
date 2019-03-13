6206e84fb8530f09e2744fb23a65a2bec6bbb73b
Jacob Ferriero <jferriero@google.com>
2019-03-13 13:01:27 -0600

    correcting spaces in variables.tf

--- examples/dataproc-persistent-history-server/terraform/variables.tf
+++ examples/dataproc-persistent-history-server/terraform/variables.tf
@@ -1,32 +1,32 @@
 variable "project" {
-  description = "Project ID for your history cluster"
+    description = "Project ID for your history cluster"
 }
 
 variable "history-server" {
-  description = "Cluster ID for the Spark / YARN history server"
+    description = "Cluster ID for the Spark / YARN history server"
 }
 
 variable "history-region" {
-  description = "GCP Compute Region for your history server and bucket"
-  default     = "us-central1"
+    description = "GCP Compute Region for your history server and bucket"
+    default     = "us-central1"
 }
 
 variable "history-bucket" {
-  description = "The bucket name for your YARN logs and Spark event logs"
+    description = "The bucket name for your YARN logs and Spark event logs"
 }
 
 variable "network" {
-  description = "The network that your hadoop clusters should use"
+    description = "The network that your hadoop clusters should use"
 }
 
 variable "data-eng-cidr-range" {
-  description = "CIDR range for the IPs that should be able to access the hadoop admin UIs. This should be a range of IPs owned by your organization."
+    description = "CIDR range for the IPs that should be able to access the hadoop admin UIs. This should be a range of IPs owned by your organization."
 }
 
 variable "hadoop-cidr-range" {
-  description = "CIDR range for the hadooop subnetwork."
+    description = "CIDR range for the hadooop subnetwork."
 }
 
 variable "hadoop-subnet" {
-  description = "Name for hadoop subnetwork"
+    description = "Name for hadoop subnetwork"
 }
