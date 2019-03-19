e83b0dcfe50ab79c0ed77039c6d8a3483c4a7491
Anna Rudy <annarudy@google.com>
2019-03-18 21:00:19 -0500

    fixing record return error

--- examples/dataflow-data-generator/data-generator-pipeline/data_generator/ParquetUtil.py
+++ examples/dataflow-data-generator/data-generator-pipeline/data_generator/ParquetUtil.py
@@ -36,8 +36,8 @@ def get_pyarrow_translated_schema(string_schema):
             nullable = False if field_mode == 'REQUIRED' else True
             pa_field = pa.field(
                 name=field_name,
-                type=converted_field_type,
-                nullable=nullable
+                type=converted_field_type
+            #    nullable=nullable
             )
             pa_schema_list.append(pa_field)
 
@@ -78,4 +78,4 @@ def fix_record_for_parquet(record, schema):
                 record[field_name]
             )
 
-    return record
+    return [record]
