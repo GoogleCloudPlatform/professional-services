2a3ec2fa2f2a1ce7c2cbda1b2568273f58602a71
Jacob Ferriero <jferriero@google.com>
2019-03-27 15:40:08 +0000

    cleaning variables.

--- examples/dataproc-persistent-history-server/terraform/variables.tf
+++ examples/dataproc-persistent-history-server/terraform/variables.tf
@@ -2,12 +2,16 @@ variable "project" {
   description = "Project ID for your history cluster"
 }
 
+variable "history-bucket" {
+  description = "The bucket name for your YARN logs and Spark event logs"
+}
+
 variable "history-server" {
   description = "Cluster ID for the Spark / YARN history server"
   default     = "history-server"
 }
 
-variable "history-server" {
+variable "long-running-cluster" {
   description = "Cluster ID for a long running dataproc cluster which will persist logs on gcs"
   default     = "long-running-cluster"
 }
@@ -17,9 +21,6 @@ variable "history-region" {
   default     = "us-central1"
 }
 
-variable "history-bucket" {
-  description = "The bucket name for your YARN logs and Spark event logs"
-}
 
 variable "network" {
   description = "The network to create that your hadoop clusters should use"
