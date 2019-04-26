82f2426a866783427e82a520a24c14f9ff919ae0
Jacob Ferriero <jferriero@google.com>
2019-04-26 14:01:11 -0400

    patch issue with parsing gcs path

--- examples/dataflow-data-generator/data-generator-pipeline/data_generator/PrettyDataGenerator.py
+++ examples/dataflow-data-generator/data-generator-pipeline/data_generator/PrettyDataGenerator.py
@@ -819,7 +819,7 @@ def write_n_line_file_to_gcs(project, temp_location, n):
         n: An integer specifying the number of lines to write to a file.
     """
     # Prepare to write gcs file 'temp_num_records.txt' in the temp_location.
-    bucket_name, path = temp_location.strip('gs://').split('/', 1)
+    bucket_name, path = temp_location.replace('gs://', '').split('/', 1)
 
     gcs_client = gcs.Client(project=project)
     temp_bucket = gcs_client.get_bucket(bucket_name)
