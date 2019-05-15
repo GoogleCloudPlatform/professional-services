4faab7e10c8c19cf6d487fc0ab5d20b9ce5b2038
Justin Mound <jmound@google.com>
2019-05-15 12:19:08 -0400

    updating tests

--- tools/terraform-module-update-scanner/tests/tests.tf
+++ tools/terraform-module-update-scanner/tests/tests.tf
@@ -3,7 +3,7 @@ module "from_github_over_https_outdated_ref" {
   source = "github.com/hashicorp/terraform?ref=v0.10.0"
 }
 module "from_github_over_https_latest_ref" {
-  source = "github.com/hashicorp/terraform?ref=v0.12.0-alpha4"
+  source = "github.com/hashicorp/terraform?ref=v0.12.0-rc1"
 }
 module "from_github_over_https_no_ref" {
   source = "github.com/hashicorp/terraform"
@@ -12,7 +12,7 @@ module "from_github_over_ssh_no_ref" {
   source = "git@github.com:hashicorp/terraform.git"
 }
 module "from_github_over_ssh_latest_ref" {
-  source = "git@github.com:hashicorp/terraform.git?ref=v0.12.0-alpha4"
+  source = "git@github.com:hashicorp/terraform.git?ref=v0.12.0-rc1"
 }
 module "from_github_over_ssh_outdated_ref" {
   source = "git@github.com:hashicorp/terraform.git?ref=v0.10.0"
@@ -22,9 +22,13 @@ module "from_github_over_ssh_outdated_ref" {
 module "bitbucket_https_no_tag" {
   source = "bitbucket.org/atlassian/atlaskit-mk-2.git"
 }
-module "bitbucket_https_latest_tag" {
+module "bitbucket_https_outdated_prefixed_tag" {
   source = "bitbucket.org/atlassian/atlaskit-mk-2.git?ref=@atlaskit/util-service-support@3.0.5"
 }
+module "bitbucket_https_latest_prefixed_tag" {
+  source = "bitbucket.org/atlassian/atlaskit-mk-2.git?ref=@atlaskit/util-service-support@4.0.3"
+}
+
 module "bitbucket_https_outdated_tag" {
   source = "bitbucket.org/atlassian/atlaskit-mk-2.git?ref=@atlaskit/adf-schema@1.3.0"
 }
@@ -37,7 +41,7 @@ module "from_github_over_https_old_ref" {
   source = "git::https://github.com/hashicorp/terraform.git?ref=v0.11.10"
 }
 module "from_github_over_https_latest_ref" {
-  source = "git::https://github.com/hashicorp/terraform.git?ref=v0.12.0-alpha4"
+  source = "git::https://github.com/hashicorp/terraform.git?ref=v0.12.0-rc1"
 }
 
 # git ssh
@@ -47,8 +51,11 @@ module "git_ssh_latest" {
 module "git_ssh_outdated" {
   source = "git::ssh://git@github.com/GoogleCloudPlatform/terraform-google-lb-http.git?ref=1.0.1"
 }
-module "module_subdir_example" {
+module "module_subdir_outdated_example" {
   source = "git::ssh://git@github.com/hashicorp/terraform.git//modules/cloud?ref=v0.12.0-alpha4"
+}
+module "module_subdir_latest_example" {
+  source = "git::ssh://git@github.com/hashicorp/terraform.git//modules/cloud?ref=v0.12.0-rc1"
 }
 
 # Comments should be ignored:
