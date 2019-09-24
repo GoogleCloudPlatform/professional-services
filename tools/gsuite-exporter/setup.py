d36d8860cceb42ba5d12af778fae765dde759e5f
Olivier Cervello <ocervello@google.com>
2019-09-24 14:22:18 +0200

    Update version

--- tools/gsuite-exporter/setup.py
+++ tools/gsuite-exporter/setup.py
@@ -31,7 +31,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='gsuite-exporter',
-    version='0.0.2',
+    version='0.0.3',
     description='GSuite Admin API Exporter',
     long_description=long_description,
     author='Google Inc.',
