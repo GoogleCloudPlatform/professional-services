12f3ab7c9f20f9f8ae8a0747f848e0f2b3b2c00b
Adrien Walkowiak <awalko@google.com>
2019-10-11 10:24:38 -0500

    Adding support for go in Cloudbuild CI testing

--- helpers/check_format.sh
+++ helpers/check_format.sh
@@ -20,30 +20,54 @@
 # The following languages are currently supported:
 # - python (using yapf)
 
-# temporary list of folders to exclude
-EXCLUDE_FOLDERS=$(cat helpers/exclusion_list.txt)
+# need_formatting - helper function to error out when
+# a folder contains files that need formatting
+# @args $1 - Folder local path
+# @args $2 - List of files in that folder that need formatting
+# Exit with error code 1 - always
+need_formatting() {
+    FOLDER=$1
+    FILES_TO_LINT=$2
+    echo "Some files need to be formatted in $FOLDER - FAIL"
+    echo "$FILES_TO_LINT"
+    exit 1
+}
 
-for FOLDER in $(find tools examples -maxdepth 1 -mindepth 1 -type d);
-do
-    if  [[ ! ${EXCLUDE_FOLDERS[@]} =~ "$FOLDER" ]]
+# validate_python - takes a folder path as input and validate python files
+# using yapf (supports both python2 and python3)
+# errors out if yapf --diff -r --style google returns a non-0 status
+validate_python() {
+    FOLDER=$1
+    echo "Validating $FOLDER - Checking python files"
+
+    FILES_TO_CHECK=$(find $FOLDER -type f -name "*.py")
+
+    # Initialize FILES_TO_LINT to empty string
+    FILES_TO_LINT=""
+
+    if [[ ! -z "$FILES_TO_CHECK" ]]
     then
-        echo "Validating $FOLDER - Checking python files"
+        # Checking python files
+        # python 2 yapf
+        echo "Testing formatting for python2 files in $FOLDER"
 
-        FILES_TO_CHECK=$(find $FOLDER -type f -name "*.py")
+        # Getting the list of files to lint
+        YAPF_PYTHON2_OUTPUT=$(python2 /usr/local/bin/yapf --diff -r --style google $FILES_TO_CHECK 2>&1)
+        YAPF_PYTHON2_STATUS=$(echo $?)
+        FILES_TO_LINT+=$( echo $YAPF_PYTHON2_OUTPUT | egrep '^---.*\(original\)$' | awk '{print $2}')
 
-        # Initialize FILES_TO_LINT to empty string
-        FILES_TO_LINT=""
-
-        if [[ ! -z "$FILES_TO_CHECK" ]]
+        if [[ ! -z "$FILES_TO_LINT" ]]
         then
-            # Checking python files
-            # python 2 yapf
-            echo "Testing formatting for python2 files in $FOLDER"
+            # Error out with details
+            need_formatting $FOLDER $FILES_TO_LINT
+        fi
 
-            # Getting the list of files to lint
-            YAPF_PYTHON2_OUTPUT=$(python2 /usr/local/bin/yapf --diff -r --style google $FILES_TO_CHECK 2>&1)
-            YAPF_PYTHON2_STATUS=$(echo $?)
-            FILES_TO_LINT+=$( echo $YAPF_PYTHON2_OUTPUT | egrep '^---.*\(original\)$' | awk '{print $2}')
+        # Checking python files if python2 failed (i.e not python2 compatible code)
+        if [[ "$YAPF_PYTHON2_STATUS" -ne 0 ]]
+        then
+            # python 3 yapf
+            echo "Testing formatting for python3 files in $FOLDER"
+            FILES_TO_LINT+=$(python3 /usr/local/bin/yapf --diff -r --style google $FILES_TO_CHECK | egrep '^---.*\(original\)$' | awk '{print $2}')
 
             if [[ ! -z "$FILES_TO_LINT" ]]
             then
@@ -52,27 +76,45 @@ do
                 exit 1
             fi
 
-            # Checking python files if python2 failed (i.e not python2 compatible code)
-            if [[ "$YAPF_PYTHON2_STATUS" -ne 0 ]]
+            if [[ -z "$FILES_TO_LINT" ]]
             then
-                # python 3 yapf
-                echo "Testing formatting for python3 files in $FOLDER"
-                FILES_TO_LINT+=$(python3 /usr/local/bin/yapf --diff -r --style google $FILES_TO_CHECK | egrep '^---.*\(original\)$' | awk '{print $2}')
-
-                if [[ ! -z "$FILES_TO_LINT" ]]
-                then
-                    echo "Some files need to be formatted in $FOLDER - FAIL"
-                    echo "$FILES_TO_LINT"
-                    exit 1
-                fi
-
-                if [[ -z "$FILES_TO_LINT" ]]
-                then
-                    echo "No files need to be formatted in $FOLDER - PASS"
-                fi
+                echo "No files need to be formatted in $FOLDER - PASS"
             fi
-        else
-            echo "No python files found for $FOLDER - PASS"
         fi
+    else
+        echo "No python files found for $FOLDER - SKIP"
+    fi
+}
+
+# validate_go - takes a folder path as input and validate go files
+# using gofmt
+# errors out if gofmt returns a non-0 status
+validate_go() {
+    FOLDER=$1
+    echo "Validating $FOLDER - Checking GO files"
+
+    FILES_TO_LINT=$(gofmt -l $FOLDER)
+
+    if [[ ! -z "$FILES_TO_LINT" ]]
+    then
+        # Error out with details
+        need_formatting $FOLDER $FILES_TO_LINT
+    else
+        echo "No go files found for $FOLDER - SKIP"
+    fi
+}
+
+# temporary list of folders to exclude
+EXCLUDE_FOLDERS=$(cat helpers/exclusion_list.txt)
+
+for FOLDER in $(find tools examples -maxdepth 1 -mindepth 1 -type d);
+do
+    if  [[ ! ${EXCLUDE_FOLDERS[@]} =~ "$FOLDER" ]]
+    then
+        validate_python $FOLDER
+        validate_go $FOLDER
+    else
+        echo "$FOLDER in exclusion list - SKIP  "
     fi
 done
+
