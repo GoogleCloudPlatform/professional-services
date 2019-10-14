b185fc280f4bf7dab4a9dca9802419c7b86061bd
Kaitlin <kardiff@google.com>
2019-10-14 14:34:45 -0400

    deleting output directories

--- examples/ml-audio-content-profiling/app/angular/node_modules/.bin/atob
+++ examples/ml-audio-content-profiling/app/angular/node_modules/.bin/atob
@@ -1,6 +1,1 @@
-#!/usr/bin/env node
-'use strict';
-
-var atob = require('../node-atob');
-var str = process.argv[2];
-console.log(atob(str));
+../atob/bin/atob.js
--- examples/ml-audio-content-profiling/app/python/main.py
+++ examples/ml-audio-content-profiling/app/python/main.py
@@ -155,4 +155,4 @@ API.add_resource(Entities, '/entities')
 API.add_resource(Analysis, '/analysis')
 
 if __name__ == '__main__':
-    APP.run(host='127.0.0.1', port=8080, debug=True)
+    APP.run(host='127.0.0.1', port=8081, debug=True)
