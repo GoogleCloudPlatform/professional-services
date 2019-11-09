8263994b1ae3a15852418f3ecc76ac8f094719e6
bmenasha <bmenasha@google.com>
2019-11-09 17:38:37 -0500

    Never convert properties to a timestamp. (#344)
    This is a backwards incompatible change and will break loads with existing
    schemas that use timestamp types for some properties. Sorry.
    
    Previously we would attempt to recognize when a value is a date or timestamp and
    use the proper BigQuery type. Unfortuantly these types are not declared in our
    API schema and it's possible for the values to be null breaking type inference.
    This then caused BigQuery loads to fail inconsistently. This fix removes the
    type timestamp inference so we only use detect bool and numberic types which are
    declared by GCP api discovery documents.
    
    Other fixes with this checkin include:
    
    - Upgrade to latest Beam SDK and associated libraires.
    - Make it easier to use pip to install test dependencies.
    - Describe how to supply num_shards paramerer in README.md (Issue #333).

--- tools/asset-inventory/setup.py
+++ tools/asset-inventory/setup.py
@@ -38,11 +38,13 @@ setup(
     keywords='gcp asset inventory',
     packages=['asset_inventory'],
     setup_requires=['pytest-runner'],
-    tests_require=['mock<3.0.0', 'pytest', 'apache-beam[gcp]>=2.9.0'],
+    extras_require = {
+      'testing': ['mock==2.0.0', 'pytest==4.6.6', 'apache-beam[gcp]==2.16.0'],
+    },
     include_package_data=True,
     data_files=[('.', ['asset_inventory/cai_to_api_properties.json'])],
     install_requires=[
-        'google-cloud-asset==0.2.0',
-        'google-cloud-bigquery<1.18.0',
+        'google-cloud-asset==0.5.0',
+        'google-cloud-bigquery==1.21.0',
         'requests-futures==1.0.0'
     ])
