a0f8e9053c362473cf02a04afffed593457e8a08
Kaitlin <kardiff@google.com>
2019-11-30 14:48:10 -0500

    remove deleted files

--- tools/gcs-usage-recommender/python/main.py
+++ tools/gcs-usage-recommender/python/main.py
@@ -26,6 +26,66 @@ def get_bq_client() -> bigquery.Client:
     return bigquery.Client()
 
 
+def table_exists(bq_client: bigquery.Client,
+                 project_id: str,
+                 dataset_id: str,
+                 table_name: str) -> bool:
+    """Determines whether table already exists or not in BigQuery.
+
+    Args:
+        bq_client: bigquery.Client object
+        project_id: String of GCP project where table should be.
+        dataset_id: String of dataset ID in BQ where table should be.
+        table_name: String of table name in BQ.
+
+    Returns:
+        Boolean indicating if table exists.
+    """
+    dataset_ref = bq_client.get_dataset(bigquery.DatasetReference(
+        project=project_id,
+        dataset_id=dataset_id))
+    table_list = list(bq_client.list_tables(dataset=dataset_ref))
+    return table_name in [table.table_id for table in table_list]
+
+
+def create_table(bq_client: bigquery.Client,
+                 project_id: str,
+                 dataset_id: str,
+                 table_name: str) -> None:
+    """Creates a table in BigQuery with specified name.
+
+    Args:
+        bq_client: bigquery.Client object
+        project_id: String of GCP project where table should be.
+        dataset_id: String of dataset ID in BQ where table should be.
+        table_name: String of table name in BQ.
+
+    Returns: None; Logs message in Stackdriver.
+    """
+    schema = [
+        bigquery.SchemaField("last_read_timestamp", "TIMESTAMP"),
+        bigquery.SchemaField("days_since_last_read", "INTEGER"),
+        bigquery.SchemaField("read_count_30_days", "INTEGER"),
+        bigquery.SchemaField("read_count_90_days", "INTEGER"),
+        bigquery.SchemaField("project_id", "STRING"),
+        bigquery.SchemaField("bucket_name", "STRING"),
+        bigquery.SchemaField("export_day", "DATE"),
+        bigquery.SchemaField("recommended_OLM", "STRING")
+    ]
+    try:
+        dataset_ref = bq_client.get_dataset(bigquery.DatasetReference(
+            project=project_id,
+            dataset_id=dataset_id))
+        table_ref = dataset_ref.table(table_name)
+        table = bigquery.Table(table_ref, schema=schema)
+        bq_client.create_table(table)
+        logging.info(f"Table {table_name} created in {project_id}.{dataset_id}")
+
+    except Exception as e:
+        logging.error(f"Failed to create table {table_name}")
+        logging.error(e)
+
+
 def file_to_string(sql_path: str) -> str:
     """Converts a SQL file holding a SQL query to a string.
 
@@ -73,6 +133,15 @@ def execute_query(bq_client: bigquery.Client, query_path: str) -> None:
 def main():
     try:
         bq_client = get_bq_client()
+        # In case someone does not do the backfill process
+        if not table_exists(bq_client,
+                            config_vars['OUTPUT_PROJECT_ID'],
+                            config_vars['OUTPUT_DATASET_ID'],
+                            config_vars['OUTPUT_TABLE_NAME']):
+            create_table(bq_client,
+                         config_vars['OUTPUT_PROJECT_ID'],
+                         config_vars['OUTPUT_DATASET_ID'],
+                         config_vars['OUTPUT_TABLE_NAME'])
         execute_query(bq_client, 'audit_log_query.sql')
 
     except Exception as e:
