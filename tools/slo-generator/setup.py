ea9a6238f85ae1f846715d952b72d923f665d85e
Olivier Cervello <ocervello@google.com>
2020-01-22 14:52:46 -0600

    Update version to 0.2.0

--- tools/slo-generator/setup.py
+++ tools/slo-generator/setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='slo-generator',
-      version='0.1.8',
+      version='0.2.0',
       description='SLO generator',
       long_description=long_description,
       long_description_content_type='text/markdown',
