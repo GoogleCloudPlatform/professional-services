3dce4adc2eed21f61af01c314ac38872f570df14
Israel Herraiz <ihr@google.com>
2020-01-30 09:39:45 +0100

    Remove extra blank line

--- examples/bigtable-change-key/scripts/copy_schema_to_new_table.sh
+++ examples/bigtable-change-key/scripts/copy_schema_to_new_table.sh
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
  
 usage() {
   echo "Usage $0 INPUT_TABLE_NAME OUTPUT_TABLE_NAME"
