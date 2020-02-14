efb56dccd263c3b482d9070ba67055e5b98dcf1f
Olivier Cervello <ocervello@google.com>
2020-02-14 10:23:33 +0100

    Improve SLO report __str__ representation

--- tools/slo-generator/slo_generator/report.py
+++ tools/slo-generator/slo_generator/report.py
@@ -260,9 +260,10 @@ class SLOReport:
         info = self.__get_info()
         slo_target_per = self.slo_target * 100
         sli_per = round(self.sli_measurement * 100, 6)
-        gap_str = str(round(self.gap, 2))
-        if self.gap > 0:
-            gap_str = f'+{self.gap}'
+        gap = round(self.gap, 2)
+        gap_str = str(gap)
+        if gap > 0:
+            gap_str = f'+{gap}'
         result_sli_str = f'SLI: {sli_per:<7} % / {slo_target_per} % ({gap_str})'
         result_str = ("Burnrate: {error_budget_burn_rate:<2} / "
                       "{alerting_burn_rate_threshold} | "
