425f9fff0fcd9deed09b8cbe27eaa23fd7ea6f41
Anna Rudy <annarudy@google.com>
2020-03-04 19:12:50 -0600

    futurizing using 2to3

--- examples/bigquery_user_info_updater_tool/bigquery_user_info_updater/tests/test_query_creator.py
+++ examples/bigquery_user_info_updater_tool/bigquery_user_info_updater/tests/test_query_creator.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
+
+
 import os
 
 from bigquery_user_info_updater.updater_tools import query_creator
