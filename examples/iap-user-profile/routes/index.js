328121b9fc1b19172fcabb68d8749450ab7bb1be
Henry Suryawirawan <hsuryawirawan@google.com>
2020-05-05 12:11:34 +0800

    Add rendering simple user info on index.mustache

--- examples/iap-user-profile/routes/index.js
+++ examples/iap-user-profile/routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+/* Users dictionary with email as key */
+const usersInfo = {}
+
+function resolveUserEmail(req) {
+  let userEmail;
+
+  if (process.env.GAE_APPLICATION) {
+    userEmail = req.header('X-Goog-Authenticated-User-Email').substring("accounts.google.com".length);
+  } else {
+    userEmail = 'henry@email.com';
+  }
+
+  return userEmail;
+}
+
+function getUserInfo(req) {
+  let userInfo;
+
+  const email = resolveUserEmail(req);
+
+  if (usersInfo[email]) {
+    userInfo = usersInfo[email];
+  } else {
+    userInfo = {email: email, fullName: 'Henry Suryawirawan'};
+  }
+
+  return userInfo;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let userInfo = getUserInfo(req);
+
+  res.render('index', userInfo);
 });
 
 module.exports = router;
