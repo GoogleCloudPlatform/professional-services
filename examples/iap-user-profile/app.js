63ee5b859437c74cd03dfa2089aa8a452bbdb2cc
Henry Suryawirawan <hsuryawirawan@google.com>
2020-05-05 12:21:50 +0800

    Remove users router

--- examples/iap-user-profile/app.js
+++ examples/iap-user-profile/app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const mustacheExpress = require('mustache-express');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
