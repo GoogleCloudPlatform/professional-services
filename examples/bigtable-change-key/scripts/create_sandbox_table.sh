704af4cd3b3085681f2aa92376bdde1ed7e9eb41
Tim Swast <swast@google.com>
2020-05-11 12:50:03 -0500

    test: add shellcheck to CI tests (#469)
    * test: add linter for shell files
    
    Also:
    
    * fix shellcheck errors in new tools/gcs2bq/run.sh
    * update contributing guide with local formatter / linter instructions
    
    * fix: lint errors with shell scripts
    
    * revert shellcheck image in cloudbuild.yaml
    
    * fix: format files in examples/ and tools/ subdirectories
    
    * fix: exclude node_modules from shellcheck
    
    * reduce width of check_format script

--- examples/bigtable-change-key/scripts/create_sandbox_table.sh
+++ examples/bigtable-change-key/scripts/create_sandbox_table.sh
@@ -37,19 +37,19 @@ fi
 # Create table
 TABLE_NAME=$1
 
-cbt createtable $TABLE_NAME
-cbt createfamily $TABLE_NAME id
-cbt createfamily $TABLE_NAME loc
+cbt createtable "$TABLE_NAME"
+cbt createfamily "$TABLE_NAME" id
+cbt createfamily "$TABLE_NAME" loc
 
 # Populate table with dummy data (3 records)
-cbt set $TABLE_NAME e57c5e6a-609f-4371-86db-158304c2c1de#189 id:ride_id=e57c5e6a-609f-4371-86db-158304c2c1de
-cbt set $TABLE_NAME e57c5e6a-609f-4371-86db-158304c2c1de#189 id:point_idx=189
-cbt set $TABLE_NAME e57c5e6a-609f-4371-86db-158304c2c1de#189 loc:latitude=40.7854
+cbt set "$TABLE_NAME" e57c5e6a-609f-4371-86db-158304c2c1de#189 id:ride_id=e57c5e6a-609f-4371-86db-158304c2c1de
+cbt set "$TABLE_NAME" e57c5e6a-609f-4371-86db-158304c2c1de#189 id:point_idx=189
+cbt set "$TABLE_NAME" e57c5e6a-609f-4371-86db-158304c2c1de#189 loc:latitude=40.7854
 
-cbt set $TABLE_NAME 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 id:ride_id=33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f
-cbt set $TABLE_NAME 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 id:point_idx=132
-cbt set $TABLE_NAME 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 loc:latitude=41.7854
+cbt set "$TABLE_NAME" 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 id:ride_id=33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f
+cbt set "$TABLE_NAME" 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 id:point_idx=132
+cbt set "$TABLE_NAME" 33cb2a42-d9f5-4b64-9e8a-b5aa1d6e142f#132 loc:latitude=41.7854
 
-cbt set $TABLE_NAME 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 id:ride_id=8fa1905e-422c-49f6-8ea3-23c579504d83
-cbt set $TABLE_NAME 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 id:point_idx=1003
-cbt set $TABLE_NAME 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 loc:latitude=11.7854
+cbt set "$TABLE_NAME" 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 id:ride_id=8fa1905e-422c-49f6-8ea3-23c579504d83
+cbt set "$TABLE_NAME" 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 id:point_idx=1003
+cbt set "$TABLE_NAME" 8fa1905e-422c-49f6-8ea3-23c579504d83#1003 loc:latitude=11.7854
