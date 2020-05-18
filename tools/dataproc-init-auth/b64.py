7ed3afe32f337c1c90971a3311e2edea605876a8
ishitashah <ishitashah@google.com>
2020-05-18 15:57:01 -0400

    Ran Flake8

--- tools/dataproc-init-auth/b64.py
+++ tools/dataproc-init-auth/b64.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import base64
 import fileinput
 import google.auth.transport.requests
 from google.oauth2 import id_token
@@ -8,7 +7,9 @@ import os
 # Verify a token on the client
 # echo "$token" | ./b64.py
 
+
 def run():
+
     buf = []
     for line in fileinput.input():
         buf.append(line.rstrip('\n'))
@@ -19,5 +20,7 @@ def run():
     payload = id_token.verify_token(token, request=request, audience=audience)
     print(payload)
 
+
 if __name__ == '__main__':
+
     run()
