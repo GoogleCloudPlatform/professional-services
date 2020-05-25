676d04ebf888926ee19dc1ed4def5cf18f772bc8
Leonid Kuligin <lkuligin@yandex.ru>
2020-05-25 16:23:57 +0200

    fixing issues and adding README

--- examples/tensorflow-unit-testing/example_test.py
+++ examples/tensorflow-unit-testing/example_test.py
@@ -1,5 +1,17 @@
-import unittest
-from unittest.mock import MagicMock
+# Copyright 2020 Google Inc. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from unittest.mock import patch
 
 import numpy as np
@@ -13,7 +25,7 @@ import example
 
 class LinearBlockFullTest(tf.test.TestCase):
     def test_basic(self):
-       testing_utils.layer_test(example.LinearBlockFull, input_shape=(4, 32))
+        testing_utils.layer_test(example.LinearBlockFull, input_shape=(4, 32))
 
     def test_output(self):
         batch_size, dim, output_dim = (3, 4, 2)
@@ -33,37 +45,34 @@ class LinearBlockTest(tf.test.TestCase):
         self.assertAllEqual(output.shape, (4, 32))
 
     def test_shape(self):
-        batch_size, dim = (4, 15)
-        x = np.ones((batch_size, dim))
-        layer = example.LinearBlock(dim)
+        batch_size, input_dim, output_dim = (4, 15, 15)
+        x = np.ones((batch_size, input_dim))
+        layer = example.LinearBlock(output_dim)
         output = layer(x)
-        self.assertAllEqual(output.shape, (batch_size, dim))
+        self.assertAllEqual(output.shape, (batch_size, output_dim))
 
     def test_shape_multidim(self):
-        batch_size, dim = (4, 15)
-        x = np.ones((batch_size, 7, dim))
-        layer = example.LinearBlock(dim)
+        batch_size, input_dim, output_dim = (4, 15, 15)
+        x = np.ones((batch_size, 7, input_dim))
+        layer = example.LinearBlock(output_dim)
         output = layer(x)
-        self.assertAllEqual(output.shape, (batch_size, 7, dim))
+        self.assertAllEqual(output.shape, (batch_size, 7, output_dim))
 
     def test_output(self):
-        dim = 4
-        batch_size = 3
-        x = np.ones((batch_size, dim))
-        layer = example.LinearBlock(dim)
+        batch_size, input_dim, output_dim = (3, 4, 4)
+        x = np.ones((batch_size, input_dim))
+        layer = example.LinearBlock(output_dim)
         output = layer(x)
         expected_output = np.array([[0.1250, -0.0576, 0.0513, -0.0305]]*batch_size)
         self.assertAllClose(output, expected_output, atol=1e-4)
 
     @patch.object(initializers, 'get', lambda _: tf.compat.v1.keras.initializers.Ones)
     def test_output_ones(self):
-        dim = 4
-        batch_size = 3
-        output_dim = 2
-        x = np.ones((batch_size, dim))
+        batch_size, input_dim, output_dim = (3, 4, 4)
+        x = np.ones((batch_size, batch_size, input_dim, output_dim))
         layer = example.LinearBlock(output_dim)
         output = layer(x)
-        expected_output = np.ones((batch_size, output_dim)) * (dim + 1)
+        expected_output = np.ones((batch_size, output_dim)) * (batch_size, input_dim, output_dim + 1)
         self.assertAllClose(output, expected_output, atol=1e-4)
 
 
@@ -81,22 +90,16 @@ class ExampleModelTest(tf.test.TestCase):
         dataset = pd.get_dummies(dataset, prefix='', prefix_sep='')
         dataset = dataset[dataset.columns].astype('float64')
         labels = dataset.pop('MPG')
-        batch_size =   10
-        df  = pd.DataFrame()
-        LABEL = ""
-        faked_dataset = tf.data.Dataset.from_tensor_slices((dict(df.pop(LABEL)), df[LABEL].values))
-        return faked_dataset.repeat().batch(batch_size)
-
         return dataset, labels
 
     def test_basic(self):
-        train_dataset, train_labels = self._get_data()
-        dim = len(train_dataset.keys())
+        train_features, train_labels = self._get_data()
+        dim = len(train_features.keys())
         example_model = example.get_model(dim)
-        test_ind = train_dataset.sample(10).index
-        test_dataset, test_labels = train_dataset.iloc[test_ind], train_labels.iloc[test_ind]
+        test_ind = train_features.sample(10).index
+        test_dataset, test_labels = train_features.iloc[test_ind], train_labels.iloc[test_ind]
         history = example_model.fit(
-            train_dataset, train_labels, steps_per_epoch=2, epochs=2,
+            train_features, train_labels, steps_per_epoch=2, epochs=2,
             batch_size=10,
             validation_split=0.1,
             validation_steps=1)
@@ -104,8 +107,8 @@ class ExampleModelTest(tf.test.TestCase):
             history.history['mse'][-1],
             history.history['loss'][-1],
             places=2)
-        _ = example_model.evaluate(test_dataset, test_labels)
-        _ = example_model.predict(test_dataset)
+        example_model.evaluate(test_dataset, test_labels)
+        example_model.predict(test_dataset)
 
 
 if __name__ == '__main__':
