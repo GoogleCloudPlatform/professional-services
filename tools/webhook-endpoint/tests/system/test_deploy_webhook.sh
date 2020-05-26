8d524068ced28050a3ff8e7c89834a2724b3ea17
Dylan Hercher <dhercher@google.com>
2020-05-26 22:39:17 +0000

    adding logic for testing E2E flow using separate commands and env

--- tools/webhook-endpoint/tests/system/test_deploy_webhook.sh
+++ tools/webhook-endpoint/tests/system/test_deploy_webhook.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # NOTE: The Webhook App Deployment requires:
 # 	- The project to exist before deployment
 # 	- App Engine must be created but no service needs to be deployed
 
-# Build Source for Webhook
-source WEBHOOK.env
+# Move to Root Directory
+cd ../.. || exit
 
 # Build Webhook Pipeline
-./build.sh
+make test_build
 
 # Send Test Data
 echo "Send Data: 1003 Records"
@@ -23,5 +37,5 @@ export RESULT_TABLE_COUNT=$(echo "SELECT COUNT(1) AS row_count FROM ${BQ_DATASET
 echo "*** Table Row Count: Expected 1003 --> Found ${RESULT_TABLE_COUNT}"
 
 # Destroy Deployment
-./tear_down.sh
+make test_destroy
 
