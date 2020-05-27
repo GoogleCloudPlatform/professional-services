7a74728df7d467a4d6f3a47f12079ce9f7561c79
Dylan Hercher <dhercher@google.com>
2020-05-27 02:00:44 +0000

    adding quotes

--- tools/webhook-endpoint/deploy_dataflow.sh
+++ tools/webhook-endpoint/deploy_dataflow.sh
@@ -26,7 +26,7 @@ export SUBSCRIPTION_PATH=projects/${PROJECT_ID}/subscriptions/${SUBSCRIPTION}
 export DATASET_TEMPLATE=${BQ_DATASET}
 export TABLE_NAME_TEMPLATE=${BQ_TABLE_TEMPLATE}
 
-gcloud beta dataflow flex-template run ${DATAFLOW_JOB_NAME} \
-        --project=${PROJECT_ID} --region=${REGION} \
-        --template-file-gcs-location=${TEMPLATE_IMAGE_SPEC} \
-        --parameters inputSubscription=${SUBSCRIPTION_PATH},outputDatasetTemplate=${DATASET_TEMPLATE},outputTableNameTemplate=${TABLE_NAME_TEMPLATE},outputDeadletterTable=${BQ_DEADLETTER}
+gcloud beta dataflow flex-template run "${DATAFLOW_JOB_NAME}" \
+        --project="${PROJECT_ID}" --region="${REGION}" \
+        --template-file-gcs-location="${TEMPLATE_IMAGE_SPEC}" \
+        --parameters inputSubscription="${SUBSCRIPTION_PATH}",outputDatasetTemplate="${DATASET_TEMPLATE}",outputTableNameTemplate="${TABLE_NAME_TEMPLATE}",outputDeadletterTable="${BQ_DEADLETTER}"
