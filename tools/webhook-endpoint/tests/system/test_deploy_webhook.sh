1cd73e3d4d9477c1f1f8bf2e0bdbdd361265be14
Dylan Hercher <dhercher@google.com>
2020-05-27 01:58:48 +0000

    fix vars

--- tools/webhook-endpoint/tests/system/test_deploy_webhook.sh
+++ tools/webhook-endpoint/tests/system/test_deploy_webhook.sh
@@ -34,10 +34,9 @@ echo "Send Data: 1003 Records"
 python3 tests/system/send_data.py
 
 # Then validate results
-export _SLEEP_SECS=300
 echo "Sleep for 300 seconds"
 sleep 300
-export RESULT_TABLE_COUNT=$(echo "SELECT COUNT(1) AS row_count FROM ${BQ_DATASET}.${BQ_TABLE_TEMPLATE};" | bq query | grep '|' | tail -n 1 | awk '{print $2;}')
+RESULT_TABLE_COUNT=$(echo "SELECT COUNT(1) AS row_count FROM ${BQ_DATASET}.${BQ_TABLE_TEMPLATE};" | bq query | grep '|' | tail -n 1 | awk '{print $2;}')
 echo "*** Table Row Count: Expected 1003 --> Found ${RESULT_TABLE_COUNT}"
 
 # Destroy Deployment
