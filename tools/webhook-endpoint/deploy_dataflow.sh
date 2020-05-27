b74332ac0a385329d939203b89f67f44427afcea
Dylan Hercher <dhercher@google.com>
2020-05-27 02:04:38 +0000

    quoting

--- tools/webhook-endpoint/deploy_dataflow.sh
+++ tools/webhook-endpoint/deploy_dataflow.sh
@@ -16,7 +16,7 @@
 
 
 # Dataflow Config Vars
-export NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
 export DATAFLOW_JOB_NAME="webhook-job-${NEW_UUID}"
 
 # Deploy Template Vars
