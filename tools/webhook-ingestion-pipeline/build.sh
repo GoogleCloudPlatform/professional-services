1ead08566280c38dbe7a27d00a0b3a3f0905c61c
Dylan Hercher <dhercher@google.com>
2020-05-28 21:06:11 +0000

    shifting to use terraforms var naming

--- tools/webhook-ingestion-pipeline/build.sh
+++ tools/webhook-ingestion-pipeline/build.sh
@@ -21,21 +21,8 @@
 cd terraform/ || exit
 echo "Apply Terraform Resources"
 terraform init
-terraform import google_app_engine_application.app "${PROJECT_ID}"
-terraform apply -auto-approve \
-	-var "project_id=${PROJECT_ID}" \
-	-var "project_num=${PROJECT_NUMBER}" \
-	-var "webhook_location=${LOCATION}" \
-	-var "webhook_app_region=${APP_REGION}" \
-	-var "webhook_region=${REGION}" \
-	-var "webhook_zone=${ZONE}" \
-	-var "gcs_bucket_name=${BUCKET_NAME}" \
-	-var "webhook_app=${DEPLOY_MANAGER_NAME}" \
-	-var "pubsub_topic=${TOPIC}" \
-	-var "pubsub_subscription=${SUBSCRIPTION}" \
-	-var "bigquery_dataset=${BQ_DATASET}" \
-	-var "bigquery_table_template=${BQ_TABLE_TEMPLATE}" \
-	-var "dead_letter_queue=${BQ_DEADLETTER}"
+terraform import google_app_engine_application.app "${TF_VAR_project_id}"
+terraform apply -auto-approve
 cd ../ || exit
 
 ./deploy_dataflow.sh
