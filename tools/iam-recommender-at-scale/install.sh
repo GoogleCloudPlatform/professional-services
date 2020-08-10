739fd5f4175e52fccd28ba9a777689ec3692e3cd
Abhishek Mishra <misabhishek@google.com>
2020-08-10 15:02:59 -0700

    adding install file

---
+++ tools/iam-recommender-at-scale/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "---------------Installing iam-recommender-at-scale---------------"
+BASEDIR=$HOME/iam-recommender-at-scale
+TEMPDIR=/tmp/professional-service
+
+rm -rf $BASEDIR
+rm -rf $TEMPDIR
+
+mkdir $BASEDIR
+wget -P $TEMPDIR https://github.com/misabhishek/professional-services/archive/master.zip
+unzip $TEMPDIR/master.zip 'professional-services-master/tools/iam-recommender-at-scale/*' -d $TEMPDIR
+mv $TEMPDIR/professional-services-master/tools/iam-recommender-at-scale/* $BASEDIR
+rm -rf $TEMPDIR
+
+cd $BASEDIR/
+
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirement.txt
