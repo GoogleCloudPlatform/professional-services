0fba3bf09fbbba1af9e2da15468c8e48d351aceb
Samet Karadag <sametkaradag@gmail.com>
2020-09-22 11:10:02 +0200

    removed unnecessary comments

--- tools/oracle2bq-schema-converter/ora2bq_convert.py
+++ tools/oracle2bq-schema-converter/ora2bq_convert.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 # Copyright 2020 Google LLC.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
