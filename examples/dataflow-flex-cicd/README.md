ba1302f6d7483ff155ed3877667f50b14bd298c3
Karim Wadie <karim.wadie@gmail.com>
2020-09-23 18:19:29 +0200

    added examples/dataflow-flex-cicd

---
+++ examples/dataflow-flex-cicd/README.md
@@ -0,0 +1,110 @@
+# Dataflow template  CICD
+
+This is a proof-of-concept for continuously deploying a Dataflow job using [Flex Template](https://cloud.google.com/dataflow/docs/guides/templates/overview#flex-templated-dataflow-jobs) and Cloud Build.
+
+It extends the work in [DataflowTemplates/flex-wordcount-python](https://github.com/GoogleCloudPlatform/DataflowTemplates/tree/master/v2/flex-wordcount-python) by adding the CICD pipeline and usage.
+
+
+## Overview
+The Wordcount pipeline demonstrates how to use the new Flex Templates
+feature in Dataflow to create a template out of practically any Dataflow pipeline. This pipeline
+does not use any [ValueProvider](https://github.com/apache/beam/blob/master/sdks/python/apache_beam/options/value_provider.py) to accept user inputs and is built like any other non-templated
+Dataflow pipeline. This pipeline also allows the user to change the job
+graph depending on the value provided for an option at runtime
+(*--format=text|avro|parquet*)
+
+We make the pipeline ready for reuse by "packaging" the pipeline artifacts (python file + [command specs](python_command_spec.json))
+in a Docker container. In order to simplify the process of packaging the pipeline into a container we
+utilize [Google Cloud Build](https://cloud.google.com/cloud-build/).
+
+We preinstall all the dependencies needed to *compile and execute* the pipeline
+into a container using a custom [Dockerfile](Dockerfile).
+
+In this example, we are using the following base image for Python 3:
+
+`gcr.io/dataflow-templates-base/python3-template-launcher-base`
+
+We will utilize Google Cloud Builds ability to build a container using a Dockerfile as documented in the [quickstart](https://cloud.google.com/cloud-build/docs/quickstart-docker).
+
+In addition, we will use a CICD pipeline on Cloud Build to update the flex template automatically. 
+
+## Dataflow Pipeline
+[WordCount Example](wordcount.py) - The Wordcount pipeline is a batch pipeline which performs a wordcount on an input text file (via --input flag) and writes the word count to GCS. The output format is determined by the value of the --format flag which can be set to either text, avro or parquet.
+
+## Getting started
+To test out the CICD pipeline end-to-end:
+
+1. Create a fork/copy of this repo on Github or Cloud Source Repositories, or start a new one.
+2. Trigger a manual or automated build as explained in the [CICD](#CICD) section.
+3. Run the template as explained in the [Running Flex Templates](#Running-flex-templates) section.
+4. Monitor the job progress in the Dataflow job history page.
+5. Make an update to your pipeline (e.g. change the name of one of the transformation step)
+6. Apply step 2 and 3 and observe change to the dataflow job through the job graph or the output. 
+
+
+## CICD
+### Steps
+The CICD pipeline is defined in [cloudbuild.yaml](cloudbuild.yaml) to be executed by Cloud Build. It follows the following steps:
+1. Clone this repo. 
+2. Build and register a container image via Cloud Build as defined in the [Dockerfile](Dockerfile). The container packages the Dataflow pipeline and its dependencies and acts as the Dataflow Flex Template
+3. Create a spec file for the created Flex Template with the container image URL
+4. Copy the spec file to GCS
+
+### Substitution variables
+Cloud Build provides default variables such as $PROJECT_ID that could be used in the build YAML file. User defined variables could also be used in the form of $_USER_VARIABLE.
+
+In this project the following variables are used:
+- $_REPO_NAME: The repo name (not URI) (e.g. dataflow-templates-cicd)
+- $_TARGET_GCR_IMAGE: The GCR image name to be submitted to Cloud Build (not URI) (e.g wordcount-flex-template) 
+- $_TEMPLATE_GCS_LOCATION: GCS location to store the template spec file (e.g. gs://bucket/dir/). The spec file path is required later on to submit run commands to Dataflow
+
+These variables must be set during manual build execution or via a build trigger
+
+### Manual builds 
+
+In the repo root directory, run the following command:
+```
+gcloud builds submit --config=cloudbuild.yaml --substitutions=_REPO_NAME="dataflow-template-cicd",_TARGET_GCR_IMAGE="word_count_flex_template_python",_TEMPLATE_GCS_LOCATION="gs://bucket/dir/"
+```
+PS: make sure that your latest code changes are pushed to the repo since the build will clone it from there and won't use the local version
+
+### Triggering builds automatically
+To trigger a build on certain actions (e.g. commits to master) 
+1. Go to Cloud Build > Triggers > Create Trigger. If you're using Github, choose the "Connect Repository" option.     
+2. Configure the trigger
+3. Point the trigger to the cloudbuild.yaml file in the repository
+4. Add the substitution variables as explained in the [Substitution variables](#substitution-variables) section.
+
+
+## Running Flex Templates
+After deploying the template, one can invoke it using this API call
+```
+API_ROOT_URL="https://dataflow.googleapis.com"
+TEMPLATES_LAUNCH_API="${API_ROOT_URL}/v1b3/projects/${PROJECT}/templates:launch"
+JOB_NAME="wordcount-flex-template-`date +%Y%m%d-%H%M%S-%N`"
+INPUT_FILE="gs://path/to/input/file"
+OUTPUT_FILE="gs://path/to/output/dir"
+FORMAT="text"
+time curl -X POST -H "Content-Type: application/json"     \
+     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+     "${TEMPLATES_LAUNCH_API}"`
+     `"?validateOnly=false"`
+     `"&dynamicTemplate.gcsPath=gs://path/to/image/spec"`
+     `"&dynamicTemplate.stagingLocation=gs://path/to/stagingLocation" \
+     -d '
+      {
+       "jobName":"'$JOB_NAME'",
+       "parameters": {
+                   "input":"'$INPUT_FILE'",
+                   "output":"'$OUTPUT_FILE'",
+                   "format":"'$FORMAT'",
+        }
+       }
+      '
+
+```
+PS: If needed, this manual step could be added in the [cloudbuild.yaml](cloudbuild.yaml) to run the latest version of the pipeline as part of the CICD
+
+## Assumptions and limitations
+1. The "clone-source-code" step in [cloudbuild.yaml](cloudbuild.yaml) is configured for a public Github repo. Private repos and/or Cloud Source Repos would require setting up authorization to access them.
+2. In complex deployments on GCP, IAM would need to be configured to grant access from/to services such as GCS, CSR, Dataflow and Cloud Build.  
---
+++ examples/dataflow-flex-cicd/wordcount.py
@@ -0,0 +1,195 @@
+#
+# Copyright (C) 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+"""A word-counting workflow using Flex Templates."""
+
+from __future__ import absolute_import
+
+import argparse
+import json
+import logging
+import re
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.io.avroio import WriteToAvro
+from apache_beam.io.parquetio import WriteToParquet
+from apache_beam.metrics import Metrics
+from apache_beam.metrics.metric import MetricsFilter
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+
+import avro.schema
+from past.builtins import unicode
+import pyarrow
+
+FORMATS = {'text', 'parquet', 'avro'}
+HEADER = ['word', 'count']
+AVRO_SCHEMA = {
+    'namespace':
+        'avro.wordcount',
+    'type':
+        'record',
+    'name':
+        'WordCount',
+    'fields': [{
+        'name': 'word',
+        'type': 'string'
+    }, {
+        'name': 'count',
+        'type': 'int'
+    }]
+}
+PARQUET_SCHEMA = pyarrow.schema([('word', pyarrow.string()),
+                                 ('count', pyarrow.int64())])
+DEFAULT_CODEC = 'snappy'
+
+
+class WordExtractingDoFn(beam.DoFn):
+  """Parse each line of input text into words."""
+
+  def __init__(self):
+    self.words_counter = Metrics.counter(self.__class__, 'words')
+    self.word_lengths_counter = Metrics.counter(self.__class__, 'word_lengths')
+    self.word_lengths_dist = Metrics.distribution(self.__class__,
+                                                  'word_len_dist')
+    self.empty_line_counter = Metrics.counter(self.__class__, 'empty_lines')
+
+  def process(self, element):
+    """Returns an iterator over the words of this element.
+
+    The element is a line of text.  If the line is blank, note that, too.
+    Args:
+      element: the element being processed
+
+    Returns:
+      The processed element.
+    """
+    text_line = element.strip()
+    if not text_line:
+      self.empty_line_counter.inc(1)
+    words = re.findall(r'[\w\']+', text_line, re.UNICODE)
+    for w in words:
+      self.words_counter.inc()
+      self.word_lengths_counter.inc(len(w))
+      self.word_lengths_dist.update(len(w))
+    return words
+
+
+def run(argv=None):
+  """Main entry point; defines and runs the wordcount pipeline."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--input',
+      dest='input',
+      default='gs://dataflow-samples/shakespeare/kinglear.txt',
+      help='Input file to process.')
+  parser.add_argument(
+      '--output',
+      dest='output',
+      required=True,
+      help='Output file to write results to.')
+  parser.add_argument(
+      '--format',
+      dest='format',
+      default='text',
+      help='Supported output file formats: %s.' % FORMATS)
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  if known_args.format not in FORMATS:
+    raise ValueError('--format should be one of: %s' % FORMATS)
+
+  # We use the save_main_session option because one or more DoFn's in this
+  # workflow rely on global context (e.g., a module imported at module level).
+  pipeline_options = PipelineOptions(pipeline_args)
+  pipeline_options.view_as(SetupOptions).save_main_session = True
+  p = beam.Pipeline(options=pipeline_options)
+
+  # Read the text file[pattern] into a PCollection.
+  lines = p | 'read' >> ReadFromText(known_args.input)
+
+  # Count the occurrences of each word.
+  def count_ones(word_ones):
+    (word, ones) = word_ones
+    return (word, sum(ones))
+
+  counts = (
+      lines
+      | 'split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(unicode))
+      | 'pair_with_one' >> beam.Map(lambda x: (x, 1))
+      | 'group' >> beam.GroupByKey()
+      | 'count' >> beam.Map(count_ones))
+
+  # Format the counts into a PCollection of strings.
+  def format_text(word_count):
+    (word, count) = word_count
+    return '%s: %d' % (word, count)
+
+  # Format the counts into a PCollection of dictionary strings.
+
+  def format_dict(word_count):
+    (word, count) = word_count
+    row = dict(zip(HEADER, [word, count]))
+    return row
+
+  if known_args.format == 'text':
+    output = counts | 'format text' >> beam.Map(format_text)
+
+    # Write the output using a "Write" transform that has side effects.
+    # pylint: disable=expression-not-assigned
+    output | 'write text' >> WriteToText(known_args.output)
+  elif known_args.format == 'avro':
+    output = counts | 'format avro' >> beam.Map(format_dict)
+
+    schema = avro.schema.parse(json.dumps(AVRO_SCHEMA))
+
+    # Write the output using a "Write" transform that has side effects.
+    # pylint: disable=expression-not-assigned
+    output | 'write avro' >> WriteToAvro(
+        file_path_prefix=known_args.output, schema=schema, codec=DEFAULT_CODEC)
+  else:
+    output = counts | 'format parquet' >> beam.Map(format_dict)
+
+    # Write the output using a "Write" transform that has side effects.
+    # pylint: disable=expression-not-assigned
+    output | 'write parquet' >> WriteToParquet(
+        file_path_prefix=known_args.output,
+        schema=PARQUET_SCHEMA,
+        codec=DEFAULT_CODEC)
+
+  result = p.run()
+  result.wait_until_finish()
+
+  # Do not query metrics when creating a template which doesn't run
+  if (not hasattr(result, 'has_job')  # direct runner
+      or result.has_job):  # not just a template creation
+    empty_lines_filter = MetricsFilter().with_name('empty_lines')
+    query_result = result.metrics().query(empty_lines_filter)
+    if query_result['counters']:
+      empty_lines_counter = query_result['counters'][0]
+      logging.info('number of empty lines: %d', empty_lines_counter.result)
+
+    word_lengths_filter = MetricsFilter().with_name('word_len_dist')
+    query_result = result.metrics().query(word_lengths_filter)
+    if query_result['distributions']:
+      word_lengths_dist = query_result['distributions'][0]
+      logging.info('average word length: %d', word_lengths_dist.result.mean)
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
+
