7e21dc5a3c074e60c55f0f2c7e14e4c2617de1c1
larchiu <llc@google.com>
2020-12-04 18:36:19 -0500

    initial checkin

---
+++ examples/cloud-datafusion-functions/src/main/java/com/google/functions/CheckPointReadFunction.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.functions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.firestore.dao.CheckpointDAO;
+
+import io.cdap.cdap.etl.api.action.ActionContext;
+
+/**
+ * This class interfaces between CheckPointReadAction and CheckpointDAO.
+ */
+public class CheckPointReadFunction {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CheckPointReadFunction.class);
+
+	private CheckpointDAO getCheckpointDAO(String serviceAccountFilePath, String projectId) {
+		com.google.firestore.dao.CheckpointDAO pipelineCheckpointDAO = new com.google.firestore.dao.CheckpointDAO(serviceAccountFilePath, projectId);
+		return pipelineCheckpointDAO;
+	}
+
+	public void execute(ActionContext context, String serviceAccountFilePath, String projectId, String collectionName, String documentName, String bufferTime) throws Exception {
+		CheckpointDAO checkpointDAO = getCheckpointDAO(serviceAccountFilePath, projectId);
+		String latestCheckpointValue = checkpointDAO.getLatestCheckpointValue(collectionName, documentName);
+		LOG.info("latestCheckpointValue == " + latestCheckpointValue);
+		if (bufferTime != null) {
+			latestCheckpointValue = latestCheckpointValue.substring(0, 19);
+			latestCheckpointValue = getWatermarkWithBufferTime(latestCheckpointValue, bufferTime);
+        	LOG.info("latestCheckpointValue after adding bufferTime == " + latestCheckpointValue);
+        }
+		if (latestCheckpointValue != null && !latestCheckpointValue.trim().equals("")) {
+			setWatermarkValueAsRuntimeArgument(context, latestCheckpointValue);
+		} else {
+			throw new RuntimeException("latestCheckpointValue is empty");
+		}
+	}
+
+	private String getWatermarkWithBufferTime(String dateStr, String bufferTime) throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = dateFormat.parse(dateStr);
+		final long ONE_MINUTE_IN_MILLIS = 60000;//millisecs
+
+	    long curTimeInMs = date.getTime();
+	    Date afterAddingMins = new Date(curTimeInMs - (Integer.parseInt(bufferTime) * ONE_MINUTE_IN_MILLIS));
+	    return dateFormat.format(afterAddingMins);
+	}
+	private void setWatermarkValueAsRuntimeArgument(ActionContext context, String formattedWatermarkValue) {
+		if (context != null) {
+			context.getArguments().set("latestWatermarkValue", formattedWatermarkValue);
+		}
+	}
+}
---
+++ examples/cloud-datafusion-functions/src/main/java/com/google/plugin/CheckPointReadAction.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.plugin;
+
+import javax.annotation.Nullable;
+
+import com.google.common.GCPConfig;
+import com.google.functions.CheckPointReadFunction;
+
+import io.cdap.cdap.api.TxRunnable;
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.api.data.DatasetContext;
+import io.cdap.cdap.etl.api.action.Action;
+import io.cdap.cdap.etl.api.action.ActionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An Action Plugin to read checkpoints stored in Firestore.
+ */
+@Plugin(type=Action.PLUGIN_TYPE)
+@Name(CheckPointReadAction.NAME)
+@Description("Reads the checkpoint details for a given table")
+public class CheckPointReadAction extends Action {
+	public static final String NAME = "CheckPointReadAction";
+	private final Conf config;
+
+	public CheckPointReadAction(Conf config) {
+		this.config = config;
+	}
+
+	@Override
+	public void run(ActionContext context) throws Exception {
+		context.execute(new TxRunnable() {
+			@Override
+			public void run(DatasetContext datasetContext) throws Exception {
+				new CheckPointReadFunction().execute(context, config.getServiceAccountFilePath(), config.getProject(),
+						config.collectionName, config.documentName, config.bufferTime);
+			}
+		});
+
+	}
+
+	public static class Conf extends GCPConfig {
+		private static final long serialVersionUID = 3838144048142671294L;
+
+		@Description("Specify the collection name in firestore DB")
+		@Macro
+		private String collectionName;
+
+		@Description("Specify the document name to read the checkpoint details")
+		@Macro
+		private String documentName;
+
+		@Description("Buffer time to add to watermark value")
+		@Macro
+		@Nullable
+		private String bufferTime;
+	}
+}
