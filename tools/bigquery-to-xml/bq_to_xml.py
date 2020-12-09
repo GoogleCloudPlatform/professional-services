d3dbb57bc019bf1895f36f15ff84b4608e74a4f6
John Mitchell <john.mitch96@gmail.com>
2020-12-09 19:37:01 +0000

    Modifying for linter

--- tools/bigquery-to-xml/bq_to_xml.py
+++ tools/bigquery-to-xml/bq_to_xml.py
@@ -14,8 +14,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-
-
 import os
 import xml.etree.ElementTree as ET
 from xml.dom.minidom import parseString
