7b8f65ab179b03a3a658e44b12d960cb0a4e9e33
Andras Gyomrey <lgandras@google.com>
2021-02-01 17:29:21 +0000

    Code review

--- tools/vpc-flowlogs-analysis/variables.tf
+++ tools/vpc-flowlogs-analysis/variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ variable "location" {
 
 variable "logs_project_id" {
   type        = string
-  description = "Id of the project where the BigQuery dataset with the VPC flowlogs will be stored."
+  description = "ID of the project where the BigQuery dataset with the VPC flowlogs will be stored."
 }
 
 variable "vpc_project_ids" {
