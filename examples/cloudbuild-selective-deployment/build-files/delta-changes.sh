aa091d517813e84a3b8b3acc8c0edc4a769348c3
Venkata Ponnam <vponnam@google.com>
2021-12-23 09:22:07 -0500

    adding license header to all tfvars files

--- examples/cloudbuild-selective-deployment/build-files/delta-changes.sh
+++ examples/cloudbuild-selective-deployment/build-files/delta-changes.sh
@@ -1,6 +1,20 @@
-#/bin/bash
-set -e
-set -o pipefail
+#!/bin/bash
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
 
 project=$1
 apply_trigger_name=$2
@@ -43,8 +57,10 @@ base_dir=$(pwd)
 logs_dir=$base_dir/logs
 projects_regex="projects\/[_a-zA-Z0-9-]*[/][_a-zA-Z0-9-]*[/]"
 user_resources_regex="user-resources\/[_a-zA-Z0-9-]*[/][_a-zA-Z0-9-]*[/]"
+
 # Make sure there is a logs dir and a file for logging.
 create_logs_dir $logs_dir  || exit 1
+
 # If manual commit sha is given, use that for the diff.
 if [ -z $manual_previous_commit_sha ] ; then
     echo "command : nth_successful_commit 1 $apply_trigger_name $project"
