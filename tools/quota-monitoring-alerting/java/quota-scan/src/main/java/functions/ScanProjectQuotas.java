030d57dc9ebaa0bc18ec5eb99b1a311d44edf08a
kimjulia1117 <43306903+kimjulia1117@users.noreply.github.com>
2022-01-04 10:29:46 -0600

    Deleted aggregation related code to get most updated quota limit

--- tools/quota-monitoring-alerting/java/quota-scan/src/main/java/functions/ScanProjectQuotas.java
+++ tools/quota-monitoring-alerting/java/quota-scan/src/main/java/functions/ScanProjectQuotas.java
@@ -8,8 +8,6 @@ import static functions.ScanProjectQuotasHelper.loadBigQueryTable;
 import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
 import com.google.cloud.monitoring.v3.MetricServiceClient.ListTimeSeriesPagedResponse;
-import com.google.monitoring.v3.Aggregation.Aligner;
-import com.google.monitoring.v3.Aggregation.Reducer;
 import com.google.monitoring.v3.ProjectName;
 import functions.eventpojos.GCPProject;
 import functions.eventpojos.GCPResourceClient;
@@ -29,15 +27,9 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
   // BigQuery Table name
   public static final String BIG_QUERY_TABLE = System.getenv("BIG_QUERY_TABLE");
 
-  // ==== Time Series Filters ====
-  // Last 7 days
-  public static final Integer DURATION = 7 * 24 * 60 * 60;
-  public static final Aligner ALIGNER = Aligner.ALIGN_MAX;
-  public static final Reducer REDUCER = Reducer.REDUCE_MAX;
-  // Last 7 days
-  public static final Integer TIME_INTERVAL_START = 7 * 24 * 60 * 60 * 1000;
-  public static final String GROUP_BY_CLAUSE_1 = "metric.quota_metric";
-  public static final String GROUP_BY_CLAUSE_2 = "resource.location";
+  //==== Time Series Filters ====
+  //Last 1 day
+  public static final Integer TIME_INTERVAL_START = 24 * 60 * 60 * 1000;
 
   private static final Logger logger = Logger.getLogger(ScanProjectQuotas.class.getName());
 
@@ -61,11 +53,11 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
       gcpProject.setProjectName(ProjectName.of(projectId).toString());
       GCPResourceClient gcpResourceClient = createGCPResourceClient();
       TimeSeriesQuery timeSeriesQuery = getTimeSeriesFilter();
-      // 1. Scan Allocation quota and load in main table in BigQuery
+      //1. Scan Allocation quota and load in main table in BigQuery
       getAllocationUsageQuotas(gcpResourceClient, timeSeriesQuery, gcpProject);
-      // 2. Scan Rate quotas and load in main table
+      //2. Scan Rate quotas and load in main table
       getRateUsageQuotas(gcpResourceClient, timeSeriesQuery, gcpProject);
-      // 3. Scan limit and load in different table
+      //3. Scan limit and load in different table
       getQuotaLimits(gcpResourceClient, timeSeriesQuery, gcpProject);
     } catch (Exception e) {
       logger.log(Level.SEVERE, " " + e.getMessage(), e);
@@ -75,11 +67,9 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
   /*
    * API to get all Allocation quotas usage for this project
    * */
-  private static void getAllocationUsageQuotas(
-      GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject) {
+  private static void getAllocationUsageQuotas(GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject){
     try {
-      scanQuota(
-          gcpResourceClient, timeSeriesQuery.getAllocationQuotaUsageFilter(), gcpProject, false);
+      scanQuota(gcpResourceClient, timeSeriesQuery.getAllocationQuotaUsageFilter(), gcpProject, false);
     } catch (IOException e) {
       logger.log(Level.SEVERE, "Error fetching Allocation usage quotas " + e.getMessage(), e);
     }
@@ -88,8 +78,7 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
   /*
    * API to get all Rate quotas usage for this project
    * */
-  private static void getRateUsageQuotas(
-      GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject) {
+  private static void getRateUsageQuotas(GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject){
     try {
       scanQuota(gcpResourceClient, timeSeriesQuery.getRateQuotaUsageFilter(), gcpProject, false);
     } catch (IOException e) {
@@ -100,8 +89,7 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
   /*
    * API to get all Quota Limits for this project
    * */
-  private static void getQuotaLimits(
-      GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject) {
+  private static void getQuotaLimits(GCPResourceClient gcpResourceClient, TimeSeriesQuery timeSeriesQuery, GCPProject gcpProject){
     try {
       scanQuota(gcpResourceClient, timeSeriesQuery.getQuotaLimitFilter(), gcpProject, true);
     } catch (IOException e) {
@@ -112,15 +100,10 @@ public class ScanProjectQuotas implements BackgroundFunction<PubSubMessage> {
   /*
    * API to get quotas from APIs and load in BigQuery
    * */
-  private static void scanQuota(
-      GCPResourceClient gcpResourceClient,
-      String filter,
-      GCPProject gcpProject,
-      Boolean isLimitData)
-      throws IOException {
-    ListTimeSeriesPagedResponse projectQuotas = getQuota(gcpProject.getProjectName(), filter);
+  private static void scanQuota(GCPResourceClient gcpResourceClient, String filter, GCPProject gcpProject, Boolean isLimitData) throws IOException {
+    ListTimeSeriesPagedResponse projectQuotas = getQuota(gcpProject.getProjectName(),filter);
     loadBigQueryTable(gcpResourceClient, projectQuotas, gcpProject.getProjectId(), isLimitData);
-    logger.log(
-        Level.INFO, "Quotas loaded successfully for project Id:" + gcpProject.getProjectId());
+    logger.log(Level.INFO, "Quotas loaded successfully for project Id:" + gcpProject.getProjectId());
   }
+
 }
