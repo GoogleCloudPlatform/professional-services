e25e7452c9619b8b1c2e15747191bd8a7cbe0d07
Christoph Grotz <christophgrotz@gmail.com>
2022-01-27 17:44:46 +0100

    Trying to fix linter warning

--- tools/service-account-provider/example_gitlab_pipeline/terraform-plan.sh
+++ tools/service-account-provider/example_gitlab_pipeline/terraform-plan.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-echo "Initing Terraform"
+echo "Planning Terraform"
 
-if [ -z "${lifetime}" ]; then
-    lifetime="https%3A%2F%2Fwww%2Egoogleapis%2Ecom%2Fauth%2Fcloud%2Dplatform"
+if [ -z "${SCOPES}" ]; then
+    SCOPES="https%3A%2F%2Fwww%2Egoogleapis%2Ecom%2Fauth%2Fcloud%2Dplatform"
 fi
-if [ -z "${lifetime}" ]; then
-    lifetime="60"
+if [ -z "${LIFETIME}" ]; then
+    LIFETIME="60"
 fi
 
-url="https://sapro.com/access?sa=${service_account}&scopes=$scopes&lifetime=${lifetime}"
-status_code=200
+STATUS_CODE=500
+url="https://sapro.com/access?sa=${SERVICE_ACCOUNT}&scopes=$SCOPES&lifetime=${LIFETIME}"
 access=$(gcloud auth print-identity-token --quiet)
 response=$(curl --silent --write-out "STATUS_CODE:%{http_code}" -H "Gitlab-Token: ${CI_JOB_JWT}" -H "Authorization: Bearer ${access}" ${url})
 token=$(echo $response | sed -e 's/.*STATUS_CODE://')
 
-if [ -n "$status_code" -a "$status_code" = '200' ]; then 
+if [ -n "$STATUS_CODE" -a "$STATUS_CODE" = '200' ]; then 
   echo "Succesfully retrieved token"
 else
-  echo "Failed retrieving token $status_code - $token"
+  echo "Failed retrieving token $STATUS_CODE - $token"
   exit 1
 fi
 
