22be8181f6bc371edb58b7182c4883b693ba6e53
Carolina Hernandez <lch@google.com>
2022-02-22 13:28:39 -0600

    Reformat using google-java-format

--- examples/dfdl-firestore-pubsub-example/src/main/java/com/example/dfdl/FirestoreService.java
+++ examples/dfdl-firestore-pubsub-example/src/main/java/com/example/dfdl/FirestoreService.java
@@ -1,19 +1,16 @@
 /**
  * Copyright 2022 Google LLC
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.example.dfdl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class FirestoreService {
 
-  @Autowired
-  private final DfdlDefRepository dfdlDefRepository;
+  @Autowired private final DfdlDefRepository dfdlDefRepository;
 
   public FirestoreService(DfdlDefRepository dfdlDefRepository) {
     this.dfdlDefRepository = dfdlDefRepository;
--- examples/dfdl-firestore-pubsub-example/src/main/java/com/example/dfdl/ProcessorService.java
+++ examples/dfdl-firestore-pubsub-example/src/main/java/com/example/dfdl/ProcessorService.java
@@ -1,19 +1,16 @@
 /**
  * Copyright 2022 Google LLC
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.example.dfdl;
 
 import java.util.Arrays;
@@ -24,9 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-/**
- *  Initializes components, configurations and services.
- */
+/** Initializes components, configurations and services. */
 @SpringBootApplication
 public class ProcessorService {
 
@@ -37,9 +32,7 @@ public class ProcessorService {
     SpringApplication.run(ProcessorService.class, args);
   }
 
-  /**
-   * Runs on start up. Retrieves all the beans that were created by the {@link ProcessorService}
-   */
+  /** Runs on start up. Retrieves all the beans that were created by the {@link ProcessorService} */
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 
