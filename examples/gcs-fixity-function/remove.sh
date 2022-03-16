b3e35ec2f63c52eee0c24aede44bc4ec766e27cf
Zef Delgadillo <zef@google.com>
2022-03-16 06:08:47 -0400

    Fix recursion bug in example/gcs-fixity-function (#790)
    Fixes bug that would recursively run the function on all cloud storage files; not just
    the files within the targeted directory.
    
    Co-authored-by: Abdel SGHIOUAR <abdelfettah@google.com>

--- examples/gcs-fixity-function/remove.sh
+++ examples/gcs-fixity-function/remove.sh
@@ -1,21 +1,30 @@
-# Copyright 2019 Google LLC
+#!/usr/bin/env bash
+
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-prepare:
-		./prepare.sh
 
-deploy:
-		./deploy.sh
+function try_and_delete {
+    gcloud functions describe "$1" &> /dev/null;
+    if [ $? -eq 1 ]; then
+        echo "Function $1 doesn't exist" >&2
+    else
+        gcloud functions delete "$1"
+    fi
+}
 
-remove:
-		./remove.sh
+try_and_delete fixity-"${BUCKET_NAME}"-deletes
+
+try_and_delete fixity-"${BUCKET_NAME}"-updates
+
+try_and_delete fixity-"${BUCKET_NAME}"-manual 
