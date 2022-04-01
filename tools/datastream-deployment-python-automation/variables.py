433ae7bac8aa6d95f124e4e8d7d8b6604ebbb154
arunsingh2803 <98982148+arunsingh2803@users.noreply.github.com>
2022-04-01 19:49:01 +0530

    Added creating private connection logic  (#801)
    * added more functionalities in datastream automation script
    
    * added more functionalities in datastream automation script
    
    Co-authored-by: Abdel SGHIOUAR <abdelfettah@google.com>

--- tools/datastream-deployment-python-automation/variables.py
+++ tools/datastream-deployment-python-automation/variables.py
@@ -17,14 +17,36 @@ This file has all the variables used by create_connection_profiles_and_datastrea
 """
 PROJECT_ID = "arunsinghk"
 GCP_LOCATION = "us-central1"
-source_profile_config = {"source_profile_name": "test-source-name",
-                         "source_profile_id": "test-source-id",
-                         "source_db_hostname": "1.2.3.4",
-                         "source_db_port": 3306,
-                         "source_db_username": "datastream"}
+
+# source_database_type can be either mysql or oracle
+source_database_type = {"db_type": "oracle"}
+
+# connection_type can be either staticServiceIpConnectivity or privateConnectivity
+source_profile_connection_type = {"connection_type": "privateConnectivity"}
+
+# If using staticServiceIpConnectivity update source_profile_config_mysql
+source_profile_config_mysql = {"source_profile_name": "test-source-name",
+                               "source_profile_id": "test-source-id",
+                               "source_db_hostname": "34.121.165.152",
+                               "source_db_port": 3306,
+                               "source_db_username": "datastream"}
+
+# If using staticServiceIpConnectivity update private_connectivity_config and private_source_profile_config_oracle
+private_connectivity_config = {"private_conn_display_name": "test123",
+                               "private_conn_id": "test123",
+                               "vpc_path": "projects/arunsinghk/global/networks/default",
+                               "subnet_range": "10.36.0.0/29"}
+
+private_source_profile_config_oracle = {"private_source_profile_name": "test-source-name",
+                                        "private_source_profile_id": "test-source-id",
+                                        "private_source_db_hostname": "10.128.0.7",
+                                        "private_source_db_port": 1521,
+                                        "private_source_db_username": "datastream",
+                                        "private_source_db_service": "ORCLDB19C"}
+
 destination_profile_config = {"destination_profile_name": "test-destination-name",
                               "destination_profile_id": "test-destination-id",
-                              "storage_bucket_name": "data-stream-script-test",
+                              "storage_bucket_name": "lets-try-oracle",
                               "storage_bucket_prefix": "/"}
 stream_config = {"stream_id": "test-stream-id",
                  "stream_name": "test-stream-name"}
