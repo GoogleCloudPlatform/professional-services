63dbe1421708dc8b082d1b57e06673efa935b277
Franklin Whaite <whaite@google.com>
2022-04-29 12:33:23 -0500

    adding license headers

--- tools/bigquery-zos-mainframe-connector/gszutil/grecv/environment/startup-script.sh
+++ tools/bigquery-zos-mainframe-connector/gszutil/grecv/environment/startup-script.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright 2022 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 echo "START TIME: $(date)"
 
 # If file already exist - no need to execute startup script (VM has been restarted not created)
