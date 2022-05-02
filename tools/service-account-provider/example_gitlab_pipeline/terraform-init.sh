267e556fc0031472a1a9424989122170da0ad42f
AlexMeissner <alexmeissner@google.com>
2022-05-02 17:56:04 +0200

    Fixed Linting Issues

--- tools/service-account-provider/example_gitlab_pipeline/terraform-init.sh
+++ tools/service-account-provider/example_gitlab_pipeline/terraform-init.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
-# shellcheck disable=SC2001
+#!/bin/bash
+# shellcheck disable=SC2086
 echo "Initing Terraform"
 
-if [ -z "${SCOPE}" ]; then
+if [[ -z "${SCOPE}" ]]; then
     SCOPE="https%3A%2F%2Fwww%2Egoogleapis%2Ecom%2Fauth%2Fcloud%2Dplatform"
 fi
-if [ -z "${LIFETIME}" ]; then
+if [[ -z "${LIFETIME}" ]]; then
     LIFETIME="60"
 fi
 
 url="https://sapro.com/access?sa=${service_account}&scopes=${SCOPE}&lifetime=${LIFETIME}"
 access=$(gcloud auth print-identity-token --quiet)
 response=$(curl --silent --write-out "STATUS_CODE:%{http_code}" -H "Gitlab-Token: ${CI_JOB_JWT}" -H "Authorization: Bearer ${access}" ${url})
-TF_VAR_ACCESS_TOKEN=$(echo "$response" | sed -e 's/STATUS_CODE\:.*//g')
-export TF_VAR_ACCESS_TOKEN
+TF_VAR_ACCESS_TOKEN_RESPONSE=$(echo "$response" | sed -e 's/STATUS_CODE\:.*//g')
+export TF_VAR_ACCESS_TOKEN=$TF_VAR_ACCESS_TOKEN_RESPONSE
 status_code=$(echo "$response" | tr -d '\n' | sed -e 's/.*STATUS_CODE://')
 
-if [ -n "$status_code" ] && [ "$status_code" = '200' ]; then 
+if [[ -n "$status_code" ]] && [[ "$status_code" = '200' ]]; then 
   echo "Succesfully retrieved token"
 else
-  echo "Failed retrieving token for ${service_account} _${status_code}_ - ${TF_VAR_access_token}"
+  echo "Failed retrieving token for ${service_account} _${status_code}_ - ${TF_VAR_ACCESS_TOKEN}"
   exit 1
 fi
 
