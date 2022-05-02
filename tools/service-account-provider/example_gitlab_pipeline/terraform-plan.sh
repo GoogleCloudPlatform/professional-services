e4daafff1f4b71c5d1a4f3416904bb45ebbea85a
AlexMeissner <alexmeissner@google.com>
2022-05-02 18:51:09 +0200

    Misc Linting Fixes

--- tools/service-account-provider/example_gitlab_pipeline/terraform-plan.sh
+++ tools/service-account-provider/example_gitlab_pipeline/terraform-plan.sh
@@ -8,11 +8,15 @@ fi
 if [[ -z "${LIFETIME}" ]]; then
     LIFETIME="60"
 fi
+if [[ -z "${service_account}" ]]; then
+    service_account="default_service_account@xyz.iam.gserviceaccount.com"
+fi
+
 
 url="https://sapro.com/access?sa=${service_account}&scopes=${SCOPE}&lifetime=${LIFETIME}"
 access=$(gcloud auth print-identity-token --quiet)
-response=$(curl --silent --write-out "STATUS_CODE:%{http_code}" -H "Gitlab-Token: ${CI_JOB_JWT}" -H "Authorization: Bearer ${access}" ${url})
-TF_VAR_ACCESS_TOKEN_RESPONSE=$(echo "$response" | sed -e 's/STATUS_CODE\:.*//g')
+response=$(curl --silent --write-out "STATUS_CODE:%{http_code}" -H "Gitlab-Token: ${CI_JOB_JWT}" -H "Authorization: Bearer ${access}" "${url}")
+TF_VAR_ACCESS_TOKEN_RESPONSE=${response/STATUS_CODE:*/}
 export TF_VAR_ACCESS_TOKEN=$TF_VAR_ACCESS_TOKEN_RESPONSE
 status_code=$(echo "$response" | tr -d '\n' | sed -e 's/.*STATUS_CODE://')
 
