838f8c78d5c8f25d7afd6bee48c411063cba2e7a
Anna Muscarella <anna@muscarella.eu>
2022-05-04 09:11:49 +0000

    fixed identation for script file

--- examples/gke-ha-setup-using-spot-vms/drain-job.sh
+++ examples/gke-ha-setup-using-spot-vms/drain-job.sh
@@ -6,20 +6,20 @@ nodes_to_drain_arr=$(kubectl get nodes --selector='!cloud.google.com/gke-spot' -
 
 
 for node_name in $${nodes_to_drain_arr[@]}; do
-# avoid using NAME headline as node name
-if [ "$node_name" != "NAME" ]; then 
-    echo "Attempting to drain $node_name from app pods"
-    kubectl drain $node_name --pod-selector=my-app-1 --delete-emptydir-data
-    kubectl drain $node_name --pod-selector=my-app-2 --delete-emptydir-data
-    kubectl drain $node_name --pod-selector=my-app-n --delete-emptydir-data
-fi
+    # avoid using NAME headline as node name
+    if [ "$node_name" != "NAME" ]; then 
+        echo "Attempting to drain $node_name from app pods"
+        kubectl drain $node_name --pod-selector=my-app-1 --delete-emptydir-data
+        kubectl drain $node_name --pod-selector=my-app-2 --delete-emptydir-data
+        kubectl drain $node_name --pod-selector=my-app-n --delete-emptydir-data
+    fi
 done
 
 # make drained nodes available for use again
 for node_name in $${nodes_to_drain_arr[@]}; do
-# avoid using NAME headline as node name
-if [ "$node_name" != "NAME" ]; then 
-    echo "Uncordoning $node_name"
-    kubectl uncordon $node_name
-fi
+    # avoid using NAME headline as node name
+    if [ "$node_name" != "NAME" ]; then 
+        echo "Uncordoning $node_name"
+        kubectl uncordon $node_name
+    fi
 done
