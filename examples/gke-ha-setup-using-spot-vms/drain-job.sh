ee8cc737e171dfb32fa334a13f3418a30059a8b7
Anna Muscarella <anna@muscarella.eu>
2022-05-05 12:35:55 +0000

    updated bash script to fix cloud build errors

--- examples/gke-ha-setup-using-spot-vms/drain-job.sh
+++ examples/gke-ha-setup-using-spot-vms/drain-job.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-# get names of all nodes which are not spot (and therefore on-demand)
-echo "Starting drain script (every 15 mins)"
-nodes_to_drain_arr=$(kubectl get nodes --selector='!cloud.google.com/gke-spot' -o=custom-columns=NAME:.metadata.name);
+# get names of all nodes which are not pre-emptible (and therefore on-demand)
+nodes_to_drain_arr=$(kubectl get nodes --selector='!cloud.google.com/gke-preemptible' -o=custom-columns=NAME:.metadata.name)
 
 
-for node_name in $${nodes_to_drain_arr[@]}; do
-    # avoid using NAME headline as node name
-    if [ "$node_name" != "NAME" ]; then 
-        echo "Attempting to drain $node_name from app pods"
-        kubectl drain $node_name --pod-selector=my-app-1 --delete-emptydir-data
-        kubectl drain $node_name --pod-selector=my-app-2 --delete-emptydir-data
-        kubectl drain $node_name --pod-selector=my-app-n --delete-emptydir-data
-    fi
+for node_name in nodes_to_drain_arr; do
+# avoid using NAME headline as node name
+  if [ "$node_name" != "NAME" ]; then 
+    echo "Attempting to drain $node_name from preemptible pods"
+    kubectl drain $node_name --pod-selector=app=core-init --delete-emptydir-data
+    kubectl drain $node_name --pod-selector=app=core-init-failed --delete-emptydir-data
+    kubectl drain $node_name --pod-selector=app=core-init-tomorrow --delete-emptydir-data
+    kubectl drain $node_name --pod-selector=app=core-default --delete-emptydir-data
+    kubectl drain $node_name --pod-selector=app=core-reader --delete-emptydir-data
+  fi
 done
 
-# make drained nodes available for use again
-for node_name in $${nodes_to_drain_arr[@]}; do
-    # avoid using NAME headline as node name
-    if [ "$node_name" != "NAME" ]; then 
-        echo "Uncordoning $node_name"
-        kubectl uncordon $node_name
-    fi
+for node_name in nodes_to_drain_arr; do
+# avoid using NAME headline as node name
+  if [ "$node_name" != "NAME" ]; then 
+    echo "Uncordoning $node_name"
+    kubectl uncordon $node_name
+  fi
 done
