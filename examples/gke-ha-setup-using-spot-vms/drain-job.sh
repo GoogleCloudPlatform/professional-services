10cb2e2aebac4e75390b44acf25e31c259643a39
Golan Parashi <golanpa@yahoo.com>
2022-06-10 15:53:11 +0300

    Update drain-job.sh
    use --no-headers  options when invoking kubectl to get list of nodes

--- examples/gke-ha-setup-using-spot-vms/drain-job.sh
+++ examples/gke-ha-setup-using-spot-vms/drain-job.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 # get names of all nodes which are not pre-spot (and therefore on-demand)
-nodes_to_drain_arr=$(kubectl get nodes --selector='!cloud.google.com/gke-spot' -o=custom-columns=NAME:.metadata.name)
+nodes_to_drain_arr=$(kubectl get nodes --no-headers --selector='!cloud.google.com/gke-spot' -o=custom-columns=NAME:.metadata.name)
 
 
 for node_name in $nodes_to_drain_arr; do
-# avoid using NAME headline as node name
-  if [ "$node_name" != "NAME" ]; then 
-    echo "Attempting to drain $node_name from spot pods"
-    kubectl drain "$node_name" --pod-selector=run=my-app --delete-emptydir-data
-  fi
+  echo "Attempting to drain $node_name from spot pods"
+  kubectl drain "$node_name" --pod-selector=run=my-app --delete-emptydir-data
 done
 
 for node_name in $nodes_to_drain_arr; do
-# avoid using NAME headline as node name
-  if [ "$node_name" != "NAME" ]; then 
-    echo "Uncordoning $node_name"
-    kubectl uncordon "$node_name"
-  fi
+  echo "Uncordoning $node_name"
+  kubectl uncordon "$node_name"
 done
