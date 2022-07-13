63aafc19ac413ea2d063329c3e59d855a776cde5
Suraj Kanojia <surajkanojia@google.com>
2022-07-14 00:02:01 +0530

    updated repo readme, added license header

--- tools/gcp-aws-ha-vpn/aws_vpn.tf
+++ tools/gcp-aws-ha-vpn/aws_vpn.tf
@@ -1,4 +1,19 @@
-
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 data "aws_vpc" "aws_vpn_network" {
   id = var.aws_vpc_id
 }
@@ -32,7 +47,7 @@ resource "aws_vpn_connection" "cx_2" {
 }
 
 resource "aws_vpn_gateway_route_propagation" "vpn_propagation" {
-  count          = var.route_table_id != "" ? 1 : 0
+  count          = var.aws_route_table_id != "" ? 1 : 0
   vpn_gateway_id = aws_vpn_gateway.vpn_gateway.id
   route_table_id = var.aws_route_table_id
 }
