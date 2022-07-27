78d0205277360850d827b68c97cf3867b5c20e1a
Bipin Upadhyaya <bipinu@google.com>
2022-07-26 21:51:14 -0400

    Format

--- examples/schedule-cloud-run-jobs/gcs-to-gcs/gcs-to-gcs.sh
+++ examples/schedule-cloud-run-jobs/gcs-to-gcs/gcs-to-gcs.sh
@@ -6,14 +6,13 @@ CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
 
 source=${GCS_SOURCE}
 destination=${GCS_DESTINATION}
-run_status=${TOPIC_NAME}
 echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
 echo "Source $source Destination $destination"
-retVal=`gsutil -m cp -r $source $destination`
+gsutil -m cp -r "$source" "$destination"
+retVal=$?
 if [[ $retVal -eq 0 ]] 
 then
     echo "Completed Task # ${CLOUD_RUN_TASK_INDEX}."
 else
     echo "Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT} failed."
 fi
-
