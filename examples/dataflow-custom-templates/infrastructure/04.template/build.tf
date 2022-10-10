53da16cdf8186bc2f6a3579feaf8a856c2991a1b
Damon Douglas <damondouglas@google.com>
2022-10-10 20:23:05 +0000

    Implement go build trigger

--- examples/dataflow-custom-templates/infrastructure/04.template/build.tf
+++ examples/dataflow-custom-templates/infrastructure/04.template/build.tf
@@ -119,3 +119,50 @@ resource "google_cloudbuild_trigger" "dataflow_template_build_trigger_python" {
     }
   }
 }
+
+// Provision Go Dataflow Custom template build trigger
+resource "google_cloudbuild_trigger" "dataflow_template_build_trigger_go" {
+  name = "dataflow-template-build-trigger-python"
+  github {
+    owner = var.github_repository_owner
+    name  = var.github_repository_name
+    push {
+      branch = var.github_repository_branch
+    }
+  }
+  build {
+    timeout = "1800s"
+
+    step {
+      dir        = "${var.working_dir_prefix}/go"
+      id         = "wordcount:build_docker_image"
+      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+      entrypoint = "gcloud"
+      args       = [
+        "builds",
+        "submit",
+        "--tag=${var.region}-docker.pkg.dev/${var.project}/${var.artifact_registry_id}/${var.image_prefix}-go"
+      ]
+    }
+
+    step {
+      id         = "wordcount:build_dataflow_batch_template-go"
+      name       = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+      entrypoint = "gcloud"
+      args       = [
+        "dataflow",
+        "flex-template",
+        "build",
+        "gs://${google_storage_bucket.dataflow_templates.name}/${var.go_application_name}-go-batch.json",
+        "--project=${var.project}",
+        "--image=${var.region}-docker.pkg.dev/${var.project}/${var.artifact_registry_id}/${var.go_application_name}-go",
+        "--metadata-file=/workspace/${var.working_dir_prefix}/infrastructure/04.template/dataflow-template.json",
+        "--sdk-language=GO",
+        "--network=${data.google_compute_network.default.name}",
+        "--subnetwork=regions/${var.region}/subnetworks/${data.google_compute_subnetwork.default.name}",
+        "--disable-public-ips",
+        "--service-account-email=${data.google_service_account.default.email}"
+      ]
+    }
+  }
+}
