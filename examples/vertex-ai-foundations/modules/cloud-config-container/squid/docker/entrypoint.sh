3e4706802f22ffe24a22e6fab756870d5f6acfad
Andrew Sallaway <andrewsal@google.com>
2022-12-07 22:32:06 -0500

    Disable shellcheck for optional use case (`which cmd`)

--- examples/vertex-ai-foundations/modules/cloud-config-container/squid/docker/entrypoint.sh
+++ examples/vertex-ai-foundations/modules/cloud-config-container/squid/docker/entrypoint.sh
@@ -19,7 +19,6 @@
 
 set -e
 
-# shellcheck disable=SC2230
 
 create_log_dir() {
   mkdir -p "${SQUID_LOG_DIR}"
@@ -39,6 +38,7 @@ create_cache_dir
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$*"
   set --
+# shellcheck disable=SC2230
 elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
   EXTRA_ARGS="${*:2}"
   set --
@@ -48,9 +48,11 @@ fi
 if [[ -z ${1} ]]; then
   if [[ ! -d "${SQUID_CACHE_DIR}"/00 ]]; then
     echo "Initializing cache..."
+    # shellcheck disable=SC2230
     $(which squid) -N -f /etc/squid/squid.conf -z
   fi
   echo "Starting squid..."
+  # shellcheck disable=SC2230
   exec "$(which squid)" -f /etc/squid/squid.conf -NYCd 1 "${EXTRA_ARGS}"
 else
   exec "$@"
