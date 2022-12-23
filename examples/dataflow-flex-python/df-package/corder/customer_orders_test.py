6788d9956ffd20b662a766f2ac70909488ba0de6
Pradeep Kumar Singh <singhpradeepk@google.com>
2022-12-23 14:11:58 +0530

    Add pydantic models

--- examples/dataflow-flex-python/df-package/corder/customer_orders_test.py
+++ examples/dataflow-flex-python/df-package/corder/customer_orders_test.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import glob
 import logging
 import os
@@ -15,17 +29,20 @@ def test_xml_to_bq():
     with TempDir() as temp_dir:
         input_file = os.path.join("../../sample-data", "customer-orders.xml")
         output_file = os.path.join(temp_dir.get_path(), "example.json")
-        extra_opts = {'input': input_file, 'output': output_file}
+        extra_opts = {
+            'input': input_file,
+            'output': output_file,
+            'dead_letter_dir': temp_dir.get_path()
+        }
         customer_orders.run(
             test_pipeline.get_full_options_as_args(**extra_opts))
-        output_files = glob.glob(output_file + '*')[0]
-
+        output_files = glob.glob(output_file + '*')
         for file_name in output_files:
             if "order" in file_name:
-                lines = open(output_file).read().splitlines()
-                assert len(lines) == 44
+                lines = open(file_name).read().splitlines()
+                assert len(lines) == 22
             if "customer" in file_name:
-                lines = open(output_file).read().splitlines()
+                lines = open(file_name).read().splitlines()
                 assert len(lines) == 4
 
 
