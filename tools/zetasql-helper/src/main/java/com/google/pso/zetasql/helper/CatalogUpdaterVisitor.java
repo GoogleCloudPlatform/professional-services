e576b143f67069ec69efdcb85e73603461776493
Pablo Paglilla <ppaglilla@google.com>
2023-03-02 21:40:26 +0000

    [zetasql-toolkit] Create CatalogUpdaterVisitor to handle catalog mutations while analyzing

---
+++ tools/zetasql-helper/src/main/java/com/google/pso/zetasql/helper/CatalogUpdaterVisitor.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2023 Google LLC All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.pso.zetasql.helper;
+
+import com.google.pso.zetasql.helper.catalog.CatalogOperations;
+import com.google.pso.zetasql.helper.catalog.CatalogWrapper;
+import com.google.pso.zetasql.helper.catalog.bigquery.ProcedureInfo;
+import com.google.pso.zetasql.helper.catalog.bigquery.TVFInfo;
+import com.google.zetasql.Function;
+import com.google.zetasql.FunctionArgumentType;
+import com.google.zetasql.FunctionSignature;
+import com.google.zetasql.SimpleColumn;
+import com.google.zetasql.SimpleTable;
+import com.google.zetasql.TVFRelation;
+import com.google.zetasql.TVFRelation.Column;
+import com.google.zetasql.TypeFactory;
+import com.google.zetasql.ZetaSQLFunctions.FunctionEnums.Mode;
+import com.google.zetasql.ZetaSQLType.TypeKind;
+import com.google.zetasql.resolvedast.ResolvedCreateStatementEnums.CreateMode;
+import com.google.zetasql.resolvedast.ResolvedCreateStatementEnums.CreateScope;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateExternalTableStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateFunctionStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateProcedureStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableAsSelectStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableFunctionStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableStmt;
+import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableStmtBase;
+import com.google.zetasql.resolvedast.ResolvedNodes.Visitor;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * ResolvedNodes.Visitor that creates catalog resources based on the ResolvedCreate*
+ * statements it visits. Supports:
+ * <ul>
+ *   <li> ResolvedCrateTableStmt
+ *   <li> ResolvedCreateTableAsSelectStmt
+ *   <li> ResolvedCreateExternalTableStmt
+ *   <li> ResolvedCreateFunctionStmt
+ *   <li> ResolvedCreateTableFunctionStmt
+ *   <li> ResolvedCreateProcedureStmt
+ * </ul>
+ */
+class CatalogUpdaterVisitor extends Visitor {
+
+  private final CatalogWrapper catalog;
+
+  /**
+   * Constructor for a CatalogUpdateVisitor.
+   *
+   * @param catalog The CatalogWrapper being used to maintain the catalog
+   */
+  public CatalogUpdaterVisitor(CatalogWrapper catalog) {
+    this.catalog = catalog;
+  }
+
+  private List<SimpleColumn> extractColumnsFromCreateTableStmt(
+      ResolvedCreateTableStmtBase resolvedCreateTableStmtBase) {
+
+    List<String> tableNamePath = resolvedCreateTableStmtBase.getNamePath();
+    String tableName = tableNamePath.get(tableNamePath.size() - 1);
+
+    return resolvedCreateTableStmtBase
+        .getColumnDefinitionList()
+        .stream()
+        .map(definition -> new SimpleColumn(
+            tableName, definition.getName(), definition.getType()
+        ))
+        .collect(Collectors.toList());
+  }
+
+  private void visitCreateTableBase(ResolvedCreateTableStmtBase createTableStmtBase) {
+    List<SimpleColumn> columns = extractColumnsFromCreateTableStmt(createTableStmtBase);
+    SimpleTable table = CatalogOperations.buildSimpleTable(
+        String.join(".", createTableStmtBase.getNamePath()),
+        columns
+    );
+
+    CreateMode createMode = createTableStmtBase.getCreateMode();
+    CreateScope createScope = createTableStmtBase.getCreateScope();
+
+    this.catalog.register(table, createMode, createScope);
+  }
+
+  /**
+   * Visits a ResolvedCreateTableStmt and creates the table in the catalog.
+   *
+   * @param createTableStmt The analyzed statement to create the table from
+   */
+  @Override
+  public void visit(ResolvedCreateTableStmt createTableStmt) {
+    this.visitCreateTableBase(createTableStmt);
+  }
+
+  /**
+   * Visits a ResolvedCreateTableAsSelectStmt and creates the table in the catalog.
+   *
+   * @param createTableAsSelectStmt The analyzed statement to create the table from
+   */
+  @Override
+  public void visit(ResolvedCreateTableAsSelectStmt createTableAsSelectStmt) {
+    this.visitCreateTableBase(createTableAsSelectStmt);
+  }
+
+  /**
+   * Visits a ResolvedCreateExternalTableStmt and creates the table in the catalog.
+   *
+   * @param createExternalTableStmt The analyzed statement to create the table from
+   */
+  @Override
+  public void visit(ResolvedCreateExternalTableStmt createExternalTableStmt) {
+    this.visitCreateTableBase(createExternalTableStmt);
+  }
+
+  /**
+   * Visits a ResolvedCreateFunctionStmt and creates the function in the catalog.
+   *
+   * @param createFunctionStmt The analyzed statement to create the function from
+   */
+  @Override
+  public void visit(ResolvedCreateFunctionStmt createFunctionStmt) {
+    Function function = new Function(
+        createFunctionStmt.getNamePath(),
+        "UDF",
+        Mode.SCALAR,
+        List.of(createFunctionStmt.getSignature())
+    );
+
+    CreateMode createMode = createFunctionStmt.getCreateMode();
+    CreateScope createScope = createFunctionStmt.getCreateScope();
+
+    catalog.register(function, createMode, createScope);
+  }
+
+  /**
+   * Visits a ResolvedCreateTableFunctionStmt and creates the TVF in the catalog.
+   *
+   * @param createTableFunctionStmt The analyzed statement to create the TVF from
+   */
+  @Override
+  public void visit(ResolvedCreateTableFunctionStmt createTableFunctionStmt) {
+    List<Column> outputSchemaColumns = createTableFunctionStmt
+        .getOutputColumnList()
+        .stream()
+        .map(resolvedOutputColumn -> Column.create(
+            resolvedOutputColumn.getName(),
+            resolvedOutputColumn.getColumn().getType())
+        )
+        .collect(Collectors.toList());
+
+    TVFInfo tvfInfo = new TVFInfo(
+        createTableFunctionStmt.getNamePath(),
+        createTableFunctionStmt.getSignature(),
+        TVFRelation.createColumnBased(outputSchemaColumns)
+    );
+
+    CreateMode createMode = createTableFunctionStmt.getCreateMode();
+
+    catalog.register(tvfInfo, createMode, CreateScope.CREATE_DEFAULT_SCOPE);
+  }
+
+  /**
+   * Visits a ResolvedCreateProcedureStmt and creates the procedure in the catalog.
+   *
+   * <p> The return type for procedures is set to TYPE_STRING rather than ARG_TYPE_VOID because
+   * the ZetaSQL analyzer cannot analyze signatures that return void.
+   *
+   * @param createProcedureStmt The analyzed statement to create the procedure from
+   */
+  @Override
+  public void visit(ResolvedCreateProcedureStmt createProcedureStmt) {
+    FunctionArgumentType returnType = new FunctionArgumentType(
+        TypeFactory.createSimpleType(TypeKind.TYPE_STRING)
+    );
+
+    FunctionSignature signature = new FunctionSignature(
+        returnType,
+        createProcedureStmt.getSignature().getFunctionArgumentList(),
+        -1
+    );
+
+    ProcedureInfo procedureInfo = new ProcedureInfo(
+        createProcedureStmt.getNamePath(),
+        signature
+    );
+
+    CreateMode createMode = createProcedureStmt.getCreateMode();
+    CreateScope createScope = createProcedureStmt.getCreateScope();
+
+    catalog.register(procedureInfo, createMode, createScope);
+  }
+
+}
--- tools/zetasql-helper/src/main/java/com/google/pso/zetasql/helper/ZetaSQLHelper.java
+++ tools/zetasql-helper/src/main/java/com/google/pso/zetasql/helper/ZetaSQLHelper.java
@@ -16,139 +16,20 @@
 
 package com.google.pso.zetasql.helper;
 
-import com.google.pso.zetasql.helper.catalog.CatalogOperations;
 import com.google.pso.zetasql.helper.catalog.CatalogWrapper;
 import com.google.pso.zetasql.helper.catalog.basic.BasicCatalogWrapper;
-import com.google.pso.zetasql.helper.catalog.bigquery.ProcedureInfo;
-import com.google.pso.zetasql.helper.catalog.bigquery.TVFInfo;
 import com.google.pso.zetasql.helper.validation.ValidatingVisitor;
 import com.google.pso.zetasql.helper.validation.ValidationError;
 import com.google.zetasql.Analyzer;
 import com.google.zetasql.AnalyzerOptions;
-import com.google.zetasql.Function;
-import com.google.zetasql.FunctionArgumentType;
-import com.google.zetasql.FunctionProtos.FunctionOptionsProto;
-import com.google.zetasql.FunctionSignature;
 import com.google.zetasql.ParseResumeLocation;
 import com.google.zetasql.SimpleCatalog;
-import com.google.zetasql.SimpleColumn;
-import com.google.zetasql.SimpleTable;
-import com.google.zetasql.TVFRelation;
-import com.google.zetasql.TVFRelation.Column;
-import com.google.zetasql.TypeFactory;
-import com.google.zetasql.ZetaSQLFunctions.FunctionEnums.Mode;
-import com.google.zetasql.ZetaSQLFunctions.SignatureArgumentKind;
-import com.google.zetasql.ZetaSQLType.TypeKind;
-import com.google.zetasql.resolvedast.ResolvedCreateStatementEnums.CreateMode;
-import com.google.zetasql.resolvedast.ResolvedCreateStatementEnums.CreateScope;
-import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateFunctionStmt;
-import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateProcedureStmt;
-import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableFunctionStmt;
-import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedCreateTableStmtBase;
 import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedStatement;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class ZetaSQLHelper {
-
-  private static List<SimpleColumn> extractColumnsFromCreateTableStmt(
-      ResolvedCreateTableStmtBase resolvedCreateTableStmtBase) {
-
-    List<String> tableNamePath = resolvedCreateTableStmtBase.getNamePath();
-    String tableName = tableNamePath.get(tableNamePath.size() - 1);
-
-    return resolvedCreateTableStmtBase
-        .getColumnDefinitionList()
-        .stream()
-        .map(definition -> new SimpleColumn(
-            tableName, definition.getName(), definition.getType()
-        ))
-        .collect(Collectors.toList());
-  }
-
-  private static void registerTableCreation(
-      CatalogWrapper catalog,
-      ResolvedCreateTableStmtBase createTableStmtBase
-  ) {
-    List<SimpleColumn> columns = extractColumnsFromCreateTableStmt(createTableStmtBase);
-    SimpleTable table = CatalogOperations.buildSimpleTable(
-        String.join(".", createTableStmtBase.getNamePath()),
-        columns
-    );
-
-    CreateMode createMode = createTableStmtBase.getCreateMode();
-    CreateScope createScope = createTableStmtBase.getCreateScope();
-
-    catalog.register(table, createMode, createScope);
-  }
-
-  private static void registerFunctionCreation(
-      CatalogWrapper catalog,
-      ResolvedCreateFunctionStmt createFunctionStmt
-  ) {
-    Function function = new Function(
-        createFunctionStmt.getNamePath(),
-        "UDF",
-        Mode.SCALAR,
-        List.of(createFunctionStmt.getSignature())
-    );
-
-    CreateMode createMode = createFunctionStmt.getCreateMode();
-    CreateScope createScope = createFunctionStmt.getCreateScope();
-
-    catalog.register(function, createMode, createScope);
-  }
-
-  private static void registerTVFCreation(
-      CatalogWrapper catalog,
-      ResolvedCreateTableFunctionStmt createTableFunctionStmt
-  ) {
-    List<Column> outputSchemaColumns = createTableFunctionStmt
-        .getOutputColumnList()
-        .stream()
-        .map(resolvedOutputColumn -> Column.create(
-            resolvedOutputColumn.getName(),
-            resolvedOutputColumn.getColumn().getType())
-        )
-        .collect(Collectors.toList());
-
-    TVFInfo tvfInfo = new TVFInfo(
-        createTableFunctionStmt.getNamePath(),
-        createTableFunctionStmt.getSignature(),
-        TVFRelation.createColumnBased(outputSchemaColumns)
-    );
-
-    CreateMode createMode = createTableFunctionStmt.getCreateMode();
-
-    catalog.register(tvfInfo, createMode, CreateScope.CREATE_DEFAULT_SCOPE);
-  }
-
-  private static void registerProcedureCreation(
-      CatalogWrapper catalog,
-      ResolvedCreateProcedureStmt createProcedureStmt
-  ) {
-    FunctionArgumentType returnType = new FunctionArgumentType(
-        TypeFactory.createSimpleType(TypeKind.TYPE_STRING)
-    );
-
-    FunctionSignature signature = new FunctionSignature(
-        returnType,
-        createProcedureStmt.getSignature().getFunctionArgumentList(),
-        -1
-    );
-
-    ProcedureInfo procedureInfo = new ProcedureInfo(
-        createProcedureStmt.getNamePath(),
-        signature
-    );
-
-    CreateMode createMode = createProcedureStmt.getCreateMode();
-    CreateScope createScope = createProcedureStmt.getCreateScope();
-
-    catalog.register(procedureInfo, createMode, createScope);
-  }
 
   public static Iterator<ResolvedStatement> analyzeStatements(
       String query, AnalyzerOptions options
@@ -176,21 +57,14 @@ public class ZetaSQLHelper {
       CatalogWrapper catalog
   ) {
     ParseResumeLocation parseResumeLocation = new ParseResumeLocation(query);
+    CatalogUpdaterVisitor catalogUpdaterVisitor = new CatalogUpdaterVisitor(catalog);
 
     return new Iterator<>() {
 
       private Optional<ResolvedStatement> previous = Optional.empty();
 
       private void applyCatalogMutation(ResolvedStatement statement) {
-        if(statement instanceof ResolvedCreateTableStmtBase) {
-          registerTableCreation(catalog, (ResolvedCreateTableStmtBase) statement);
-        } else if (statement instanceof ResolvedCreateFunctionStmt) {
-          registerFunctionCreation(catalog, (ResolvedCreateFunctionStmt) statement);
-        } else if (statement instanceof ResolvedCreateTableFunctionStmt) {
-          registerTVFCreation(catalog, (ResolvedCreateTableFunctionStmt) statement);
-        } else if (statement instanceof ResolvedCreateProcedureStmt) {
-          registerProcedureCreation(catalog, (ResolvedCreateProcedureStmt) statement);
-        }
+        statement.accept(catalogUpdaterVisitor);
       }
 
       @Override
