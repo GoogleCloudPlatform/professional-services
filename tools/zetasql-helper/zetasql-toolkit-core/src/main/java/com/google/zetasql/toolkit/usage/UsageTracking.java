df2fb9b381f7e80267588cbadd52ffb6afff762a
Pablo Paglilla <ppaglilla@google.com>
2023-04-17 03:14:56 +0000

    [zetasql-toolkit] Bump snapshot version to 0.1.3-SNAPSHOT

--- tools/zetasql-helper/zetasql-toolkit-core/src/main/java/com/google/zetasql/toolkit/usage/UsageTracking.java
+++ tools/zetasql-helper/zetasql-toolkit-core/src/main/java/com/google/zetasql/toolkit/usage/UsageTracking.java
@@ -23,7 +23,7 @@ import com.google.common.collect.ImmutableMap;
 public class UsageTracking {
 
   private static final String USER_AGENT_HEADER = "user-agent";
-  private static final String USER_AGENT_VALUE = "google-pso-tool/zetasql-toolkit/0.1.1";
+  private static final String USER_AGENT_VALUE = "google-pso-tool/zetasql-toolkit/0.1.3-SNAPSHOT";
 
   public static final HeaderProvider HEADER_PROVIDER =
       FixedHeaderProvider.create(ImmutableMap.of(USER_AGENT_HEADER, USER_AGENT_VALUE));
