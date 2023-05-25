ecce2c5229bb87a6ef4a81604b1c6e978e5afd84
Paul Balm <paulbalm@gmail.com>
2023-05-25 11:01:21 +0200

    typo

--- examples/vertex_mlops_enterprise/src/kfp_pipelines/src/config.py
+++ examples/vertex_mlops_enterprise/src/kfp_pipelines/src/config.py
@@ -4,5 +4,5 @@ MY_STAGING_BUCKET = 'pbalm-cxb-aa'
 PIPELINE_NAME = 'xgb-creditcards'
 PIPELINE_ROOT = f'gs://{MY_STAGING_BUCKET}/pipeline_root/{PIPELINE_NAME}'
 
-BQ_INPUT_DATA=f"{PROJECT_ID}.vertex_eu.creditcards"}
+BQ_INPUT_DATA=f"{PROJECT_ID}.vertex_eu.creditcards"
 PARENT_MODEL='projects/{PROJECT_NR}/locations/{REGION}}/models/7109310243804282880'
