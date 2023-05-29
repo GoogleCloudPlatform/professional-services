627788910ece6c79a3a5b0ec47f159d8aba2c1f4
hariskhan <hariskhan@google.com>
2023-05-29 18:23:43 +0000

    fixed the license

--- tools/cloud-composer-dag-generator/dag_generator/__main__.py
+++ tools/cloud-composer-dag-generator/dag_generator/__main__.py
@@ -1,18 +1,18 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
-   Copyright 20232 Google LLC
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
---------------------------------------------------------------------------
 This project is a automatic DAGS generator which is used to create workload
 into an composer environmnet and to test differents airflows configurations
 or to do fine tune using the airflow metrics.
@@ -30,6 +30,7 @@ NOTE: the "number_of_operators_defined" variable in the configuration file
         d) python_operator_task_sleep
         e) python_operator_task_print
 """
+
 import json
 import random
 import modules.initDag
