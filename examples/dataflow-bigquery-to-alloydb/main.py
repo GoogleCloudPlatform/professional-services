0b146a53e6422450ca25e3a413c9d6772a42323c
Julian Ogando <julian.ogando@gmail.com>
2023-06-02 13:49:13 +0000

    fix format

--- examples/dataflow-bigquery-to-alloydb/main.py
+++ examples/dataflow-bigquery-to-alloydb/main.py
@@ -36,86 +36,60 @@ def run(argv=None, save_main_session=True):
     """Runs the pipeline"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--alloydb_username',
-        dest='alloydb_username',
-        required=True,
-        help='AlloyDB username'
-    )
-    parser.add_argument(
-        '--alloydb_password',
-        dest='alloydb_password',
-        required=True,
-        help='AlloyDB password'
-    )
-    parser.add_argument(
-        '--alloydb_ip',
-        dest='alloydb_ip',
-        required=True,
-        help='AlloyDB IP Address'
-    )
-    parser.add_argument(
-        '--alloydb_port',
-        dest='alloydb_port',
-        default="5432",
-        help='AlloyDB Port'
-    )
-    parser.add_argument(
-        '--alloydb_database',
-        dest='alloydb_database',
-        required=True,
-        help='AlloyDB Database name'
-    )
-    parser.add_argument(
-        '--alloydb_table',
-        dest='alloydb_table',
-        required=True,
-        help='AlloyDB table name'
-    )
+    parser.add_argument('--alloydb_username',
+                        dest='alloydb_username',
+                        required=True,
+                        help='AlloyDB username')
+    parser.add_argument('--alloydb_password',
+                        dest='alloydb_password',
+                        required=True,
+                        help='AlloyDB password')
+    parser.add_argument('--alloydb_ip',
+                        dest='alloydb_ip',
+                        required=True,
+                        help='AlloyDB IP Address')
+    parser.add_argument('--alloydb_port',
+                        dest='alloydb_port',
+                        default="5432",
+                        help='AlloyDB Port')
+    parser.add_argument('--alloydb_database',
+                        dest='alloydb_database',
+                        required=True,
+                        help='AlloyDB Database name')
+    parser.add_argument('--alloydb_table',
+                        dest='alloydb_table',
+                        required=True,
+                        help='AlloyDB table name')
     parser.add_argument(
         '--bq_query',
         dest='bq_query',
         required=True,
-        help='Query to be executed by BigQuery for extracting the source data'
-    )
+        help='Query to be executed by BigQuery for extracting the source data')
     known_args, pipeline_args = parser.parse_known_args(argv)
 
     pipeline_options = PipelineOptions(pipeline_args)
-    pipeline_options.view_as(
-        SetupOptions
-        ).save_main_session = save_main_session
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
     with beam.Pipeline(options=pipeline_options) as p:
-        coders.registry.register_coder(
-            EthereumTokenTransfersRowType,
-            coders.RowCoder
-        )
+        coders.registry.register_coder(EthereumTokenTransfersRowType,
+                                       coders.RowCoder)
 
-        (
-            p | 'ReadFromBigQuery' >> beam.io.ReadFromBigQuery(
-                query=known_args.bq_query,
-                use_standard_sql=True
-            )
-            | 'ConvertToRows' >> beam.Map(lambda bq_row: beam.Row(
-                    from_address=bq_row['from_address'],
-                    to_address=bq_row['to_address'],
-                    value=bq_row['value'],
-                    block_timestamp=bq_row['block_timestamp'].isoformat()
-            )
-            ).with_output_types(EthereumTokenTransfersRowType)
-            | 'Write to jdbc' >> WriteToJdbc(
-                    driver_class_name='org.postgresql.Driver',
-                    table_name=known_args.alloydb_table,
-                    jdbc_url=(
-                        f'jdbc:postgresql://{known_args.alloydb_ip}:'
-                        f'{known_args.alloydb_port}'
-                        f'/{known_args.alloydb_database}'
-                    ),
-                    username=known_args.alloydb_username,
-                    password=known_args.alloydb_password,
-                    connection_properties='stringtype=unspecified'
-            )
-        )
+        (p | 'ReadFromBigQuery' >> beam.io.ReadFromBigQuery(
+            query=known_args.bq_query, use_standard_sql=True) |
+         'ConvertToRows' >> beam.Map(lambda bq_row: beam.Row(
+             from_address=bq_row['from_address'],
+             to_address=bq_row['to_address'],
+             value=bq_row['value'],
+             block_timestamp=bq_row['block_timestamp'].isoformat()
+         )).with_output_types(EthereumTokenTransfersRowType) | 'Write to jdbc'
+         >> WriteToJdbc(driver_class_name='org.postgresql.Driver',
+                        table_name=known_args.alloydb_table,
+                        jdbc_url=(f'jdbc:postgresql://{known_args.alloydb_ip}:'
+                                  f'{known_args.alloydb_port}'
+                                  f'/{known_args.alloydb_database}'),
+                        username=known_args.alloydb_username,
+                        password=known_args.alloydb_password,
+                        connection_properties='stringtype=unspecified'))
 
 
 if __name__ == '__main__':
