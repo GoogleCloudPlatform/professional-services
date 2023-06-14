be3b23e6daebcf7b5d46374b45a9fdd94f2963c5
Oscar Pulido <oscarpulido@google.com>
2023-06-14 11:09:01 -0500

    Bash script enhanced

--- examples/personal-workbench-notebooks-deployer/modules/personal-user-managed-notebook/template_generator.sh
+++ examples/personal-workbench-notebooks-deployer/modules/personal-user-managed-notebook/template_generator.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 zone=$(gcloud compute instances list --filter="name=$(hostname)" --format "get(zone)" | awk -F/ '{print $NF}')
-user_pre=$(gcloud compute instances describe $(hostname) --zone=$zone --flatten="metadata[proxy-user-mail]")
+user_pre=$(gcloud compute instances describe "$(hostname)" --zone=$zone --flatten="metadata[proxy-user-mail]")
 user=$(echo $user_pre | cut -c5-1000)
-bucket_pre=$(gcloud compute instances describe $(hostname) --zone=$zone --flatten="metadata[templates_bucket_name]")
+bucket_pre=$(gcloud compute instances describe "$(hostname)" --zone=$zone --flatten="metadata[templates_bucket_name]")
 bucket=$(echo $bucket_pre | cut -c5-1000)
-templatesrc_pre=$(gcloud compute instances describe $(hostname) --zone=$zone --flatten="metadata[master_templates_path_name]")
+templatesrc_pre=$(gcloud compute instances describe "$(hostname)" --zone=$zone --flatten="metadata[master_templates_path_name]")
 templatesrc=$(echo $templatesrc_pre | cut -c5-1000)
-templatedest_pre=$(gcloud compute instances describe $(hostname) --zone=$zone --flatten="metadata[generated_templates_path_name]")
+templatedest_pre=$(gcloud compute instances describe "$(hostname)" --zone=$zone --flatten="metadata[generated_templates_path_name]")
 templatedest=$(echo $templatedest_pre | cut -c5-1000)
-yamlname_pre=$(gcloud compute instances describe $(hostname) --zone=$zone --flatten="metadata[dataproc_yaml_template_file_name]")
+yamlname_pre=$(gcloud compute instances describe "$(hostname)" --zone=$zone --flatten="metadata[dataproc_yaml_template_file_name]")
 yamlname=$(echo $yamlname_pre | cut -c5-1000)
 
 gsutil cp gs://$bucket/$templatesrc/$yamlname .
