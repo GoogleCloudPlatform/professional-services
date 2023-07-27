ff76f4be85eeb639d853ca8bd0a3d5a1571fa1f5
harish1203 <harishsridhar@google.com>
2023-07-27 18:14:17 +0530

    Summary and detailed report changes
    Summary report made as default.Dag's info added to summary and detailed report added with all the change info

--- tools/airflow-v1-to-v2-migration/airflow_migration/migration.py
+++ tools/airflow-v1-to-v2-migration/airflow_migration/migration.py
@@ -44,16 +44,17 @@ class MigrationUtility:
                                                          clean_input(col[4]), clean_input(col[5]), clean_input(col[6]))
 
     # Function to generate summary report
-    def generate_summary_report(self, total_num_dag, total_change_num, imp_num, imp_op_num, imp_op_arg_num):
+    def generate_summary_report(self, total_num_dag, total_change_num, imp_num, imp_op_num, imp_op_arg_num,
+                                impacted_imp_files, impacted_imp_operator_files, impacted_imp_operator_arg_files):
         summary_report = PrettyTable()
         summary_report.title = "SUMMARY REPORT"
-        summary_report.field_names = ["DESCRIPTION", "COUNT"]
+        summary_report.field_names = ["DESCRIPTION", "INFO"]
 
         # Set the alignment of columns
         summary_report.align["DESCRIPTION"] = "l"
-        summary_report.align["COUNT"] = "l"
+        summary_report.align["INFO"] = "l"
 
-        # Adding each value to the table
+        # Adding count of the files
         summary_report.add_row(["Total number of DAG's", total_num_dag])
         summary_report.add_row(["Total number of DAG's with changes: ", total_change_num])
         summary_report.add_row(["Total number of DAG's with import changes: ", imp_num])
@@ -61,30 +62,40 @@ class MigrationUtility:
         summary_report.add_row(
             ["Total number of DAG's with import, operator and argument changes: ", imp_op_arg_num])
 
+        # Add blank rows to create spacing
+        for _ in range(2):
+            summary_report.add_row(["", ""])
+
+        # Adding info on the files
+        summary_report.add_row(["Impacted DAG's with import changes", [file for file in impacted_imp_files]])
+        summary_report.add_row(["_" * 20, "_" * 20])
+        summary_report.add_row(["Impacted DAG's with import and operator changes",
+                                      [file for file in impacted_imp_operator_files]])
+        summary_report.add_row(["_" * 20, "_" * 20])
+        summary_report.add_row(["Impacted DAG's with import, operator and argument changes",
+                                      [file for file in impacted_imp_operator_arg_files]])
+
         # Generate the empty report and add the table to the report
         summary_report_file = f"{self.output_dir}/Summary-Report.txt"
         with open(summary_report_file, "w") as sum_report:
             sum_report.write(str(summary_report))
 
     # Function to generate Detailed report
-    def generate_detailed_report(self, impacted_imp_files, impacted_imp_operator_files, impacted_imp_operator_arg_files):
+    def generate_detailed_report(self, line_changes):
         detailed_report = PrettyTable()
         detailed_report.title = "DETAILED REPORT"
-        detailed_report.field_names = ["DESCRIPTION", "DAG FILE"]
+        detailed_report.field_names = ["DAG FILE", "AUTOMATED", "CHANGE_TYPE", "LINE_NO", "OLD_STATEMENT", "NEW_STATEMENT"]
 
         # Set the alignment of columns
-        detailed_report.align["DESCRIPTION"] = "l"
-        detailed_report.align["DAG FILE"] = "l"
-        detailed_report.max_table_width = 75
+        # detailed_report.align["DESCRIPTION"] = "l"
+        # detailed_report.align["DAG FILE"] = "l"
+        detailed_report.align["DAG FILE"], detailed_report.align["AUTOMATED"], detailed_report.align["CHANGE_TYPE"], detailed_report.align["LINE_NO"], \
+            detailed_report.align["OLD_STATEMENT"], detailed_report.align["NEW_STATEMENT"] = "l", "l", "l", "l", "l", "l"
+        detailed_report.max_table_width = 300
 
         # Adding each value to the table
-        detailed_report.add_row(["Impacted DAG's with import changes", [file for file in impacted_imp_files]])
-        detailed_report.add_row(["_" * 20, "_" * 20])
-        detailed_report.add_row(["Impacted DAG's with import and operator changes",
-                                 [file for file in impacted_imp_operator_files]])
-        detailed_report.add_row(["_" * 20, "_" * 20])
-        detailed_report.add_row(["Impacted DAG's with import, operator and argument changes",
-                                 [file for file in impacted_imp_operator_arg_files]])
+        for row in line_changes:
+            detailed_report.add_row(row)
 
         # Generate the empty report and add the table to the report
         detailed_report_file = f"{self.output_dir}/Detailed-Report.txt"
@@ -92,6 +103,7 @@ class MigrationUtility:
             det_report.write(str(detailed_report))
 
     def migrate_files(self):
+        # Args for summary report
         impacted_files = []
         impacted_imp_files = []
         impacted_imp_op_files = []
@@ -100,6 +112,10 @@ class MigrationUtility:
         imp_num = 0
         imp_op_num = 0
         imp_op_arg_num = 0
+
+        # Args for detailed report
+        line_changes = []
+
         for filepath in glob.iglob(f"{self.input_dir}/*.py", recursive=True):
             total_num_dag += 1
             imp_change = False
@@ -108,9 +124,10 @@ class MigrationUtility:
             change_count = 0
             filename = Path(filepath).stem
             new_file = f"{self.output_dir}/{filename}_v2.py"
+
             # Iterate through each file and open new v2 file
             with open(filepath, 'r') as f, open(new_file, 'w') as temp:
-                for line in f:
+                for line_number, line in enumerate(f, start=1):
                     # check if a comment
                     # add feature to identify multi-line comments and ignore
                     if line.startswith('#'):
@@ -127,6 +144,7 @@ class MigrationUtility:
                             if imp_stmt in self.replacement_dict:
                                 imp_change = True
                                 change_count += 1
+                                old_line = line
                                 if self.add_comments:
                                     if self.comments:
                                         comment = '# ' + self.comments + '\n'
@@ -136,6 +154,8 @@ class MigrationUtility:
                                                   self.replacement_dict[imp_stmt][3] + '\n'
                                     temp.write(comment)
                                 temp.write(self.replacement_dict[imp_stmt][2] + '\n')
+                                line_changes.append(
+                                    [filename + ".py", "Y", "Import_Change", line_number, old_line.rstrip(), self.replacement_dict[imp_stmt][2]])
                             else:
                                 temp.write(imp_stmt + '\n')
                     else:
@@ -147,6 +167,7 @@ class MigrationUtility:
                             # rule dict
                             for rec in matches:
                                 if rec in self.replacement_dict:
+                                    old_line = line
                                     change_count += 1
                                     imp_change = False
                                     imp_op_change = True
@@ -159,9 +180,12 @@ class MigrationUtility:
                                                       self.replacement_dict[rec][3] + '\n'
                                         temp.write(comment)
                                     line = line.replace(rec, self.replacement_dict[rec][2])
+                                    line_changes.append(
+                                        [filename + ".py", "Y", "Operator_Change", line_number, old_line.rstrip(), line.rstrip()])
                                     # Argument Changes - Check for a rule in rules dict with Operator name+( e.g.
                                     # BigQueryOperator(
-                                    if rec+"(" in self.replacement_dict:
+                                    if rec + "(" in self.replacement_dict:
+                                        old_line = line
                                         imp_change = False
                                         imp_op_change = False
                                         imp_op_arg_change = True
@@ -172,8 +196,8 @@ class MigrationUtility:
                                                 comment = '# ' + self.comments + '\n'
                                             else:
                                                 comment = '# Migration Utility Generated Comment -- Change Type = ' + \
-                                                      self.replacement_dict[rec+"("][1] + " , Impact = " + \
-                                                      self.replacement_dict[rec+"("][3] + '\n'
+                                                          self.replacement_dict[rec + "("][1] + " , Impact = " + \
+                                                          self.replacement_dict[rec + "("][3] + '\n'
                                             temp.write(comment)
                                         # Truncate the new line character and hold in temp variable to check if line
                                         # ends with ")" to identify if operator call is in single line if it is single
@@ -181,14 +205,21 @@ class MigrationUtility:
                                         # current line itself else add a new line with argument details from rule dict
                                         truncatedline = line.strip()
                                         if truncatedline.endswith(")"):
-                                            line = line.replace(")", ","+self.replacement_dict[rec+"("][2]+")")
+                                            line = line.replace(")", "," + self.replacement_dict[rec + "("][2] + ")")
+                                            line_changes.append(
+                                                [filename + ".py", "Y", "Argument_Change", line_number, old_line, line])
+                                            print(line_changes)
                                         else:
-                                            line = line+' '*space_count + self.replacement_dict[rec+"("][2]+",\n"
-
+                                            line = line + ' ' * space_count + self.replacement_dict[rec + "("][
+                                                2] + ",\n"
+                                            line_changes.append(
+                                                [filename + ".py", "Y", "Argument_Change", line_number, old_line.rstrip(),
+                                                 line.rstrip()])
                         temp.write(line)
 
             if change_count > 0:
-                impacted_files.append(filename+'.py')
+                impacted_files.append(filename + '.py')
+
             # Append filename with an import change
             if imp_change:
                 imp_num += 1
@@ -202,10 +233,12 @@ class MigrationUtility:
                 imp_op_arg_num += 1
                 impacted_imp_op_arg_files.append(filename + '.py')
 
+        self.generate_summary_report(total_num_dag, imp_num + imp_op_num + imp_op_arg_num, imp_num, imp_op_num,
+                                     imp_op_arg_num, impacted_imp_files, impacted_imp_op_files,
+                                     impacted_imp_op_arg_files)
         # Append all filenames when is a change
         if self.report_generation:
-            self.generate_summary_report(total_num_dag, imp_num+imp_op_num+imp_op_arg_num, imp_num, imp_op_num, imp_op_arg_num)
-            self.generate_detailed_report(impacted_imp_files, impacted_imp_op_files, impacted_imp_op_arg_files)
+            self.generate_detailed_report(line_changes)
 
 
 def run_migration(input_dag, output_dag, rules_file, add_comments, comments, report_generation):
