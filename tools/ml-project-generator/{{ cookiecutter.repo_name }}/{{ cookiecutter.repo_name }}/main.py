ccebf7a83b83c7021675ce41593fee6345c0187d
Rohit Naidu <rohitnaidu@google.com>
2024-02-25 10:20:51 +0000

    fixed linting

--- tools/ml-project-generator/{{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/main.py
+++ tools/ml-project-generator/{{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/main.py
@@ -3,33 +3,34 @@
 # agreement with Google.
 
 
-import fire
-from {{ cookiecutter.repo_name }}.config import base_config, log, logger, predict_config
+# Uncomment below Code
+# import fire
+# from {{ cookiecutter.repo_name }}.config import base_config, log, logger, predict_config
 
 
-class Executor:
-    """Runner class to call from command line with parameters."""
+# class Executor:
+#     """Runner class to call from command line with parameters."""
 
-    @log
-    def setup(self):
-        "Setup the Project & resources"
-        pass
+#     @log
+#     def setup(self):
+#         "Setup the Project & resources"
+#         pass
 
-    @log
-    def train(self):
-        "Run Pipeline to Train using train config"
-        pass
+#     @log
+#     def train(self):
+#         "Run Pipeline to Train using train config"
+#         pass
 
-    @log
-    def test(self):
-        "Run Pipeline to Test using test config"
-        pass
+#     @log
+#     def test(self):
+#         "Run Pipeline to Test using test config"
+#         pass
 
-    @log
-    def predict(self, instance):
-        "Get Predictions from the model for instance using predict config"
-        pass
+#     @log
+#     def predict(self, instance):
+#         "Get Predictions from the model for instance using predict config"
+#         pass
 
 
-if __name__ == "__main__":
-    fire.Fire(Executor)
+# if __name__ == "__main__":
+#     fire.Fire(Executor)
