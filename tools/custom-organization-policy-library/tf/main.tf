a895aad95cb0cf3636fc349ca6ea7730dcdfc7a0
Edi Wiraya <ediw@google.com>
2024-05-16 17:12:50 +0800

    fix: make file params and tf folder change

--- tools/custom-organization-policy-library/tf/main.tf
+++ tools/custom-organization-policy-library/tf/main.tf
@@ -3,6 +3,8 @@ module "custom_constraints_factory" {
   organization_id = var.organization_id
 
   factories_config = {
+    # Make sure generated tf yaml constraints and policies files copied to factory data folder.
+    # eg. data/custom-constraints
     org_policy_custom_constraints = "data/custom-constraints"
     org_policies                  = "data/custom-policies"
   }
