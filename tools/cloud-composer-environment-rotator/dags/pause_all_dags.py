ad6d30717673e30a6ded9834d304fb60868893c6
Christian Yarros <yarros@google.com>
2024-10-31 17:25:41 +0000

    formatting

--- tools/cloud-composer-environment-rotator/dags/pause_all_dags.py
+++ tools/cloud-composer-environment-rotator/dags/pause_all_dags.py
@@ -5,8 +5,7 @@ A DAG to pause all other dags.
 from datetime import datetime, timedelta
 from airflow import models
 from airflow.providers.postgres.operators.postgres import (
-    PostgresOperator,
-)
+    PostgresOperator,)
 
 default_args = {
     "owner": "auditing",
@@ -19,26 +18,24 @@ default_args = {
 }
 
 with models.DAG(
-    "pause_all_dags",
-    tags=["airflow_db"],
-    description="Pause all dags.",
-    is_paused_upon_creation=False,
-    catchup=False,
-    start_date=datetime(2024, 1, 1),
-    dagrun_timeout=timedelta(minutes=30),
-    max_active_runs=1,
-    default_args=default_args,
-    schedule_interval=None, 
+        "pause_all_dags",
+        tags=["airflow_db"],
+        description="Pause all dags.",
+        is_paused_upon_creation=False,
+        catchup=False,
+        start_date=datetime(2024, 1, 1),
+        dagrun_timeout=timedelta(minutes=30),
+        max_active_runs=1,
+        default_args=default_args,
+        schedule_interval=None,
 ) as dag:
-    
-    pause_all_dags = PostgresOperator(
-        task_id='pause_all_dags',
-        postgres_conn_id='airflow_db', 
-        sql="""
+
+    pause_all_dags = PostgresOperator(task_id='pause_all_dags',
+                                      postgres_conn_id='airflow_db',
+                                      sql="""
             UPDATE dag
             SET is_paused = true 
             WHERE 
                 dag_id != 'airflow_monitoring' AND 
                 dag_id != 'pause_all_dags';
-            """
-    )
+            """)
