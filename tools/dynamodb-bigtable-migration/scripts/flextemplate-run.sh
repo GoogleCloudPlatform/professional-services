82c1629e435c4cb6664154a8265e445e51aef9a1
mahesh goyal <maheshgoyal@google.com>
2024-12-17 10:30:45 -0600

    fix the scripts errors during the build

--- tools/dynamodb-bigtable-migration/scripts/flextemplate-run.sh
+++ tools/dynamodb-bigtable-migration/scripts/flextemplate-run.sh
@@ -3,24 +3,23 @@ set -eE
 
 script_dir=$(dirname "$0")
 
-source ${script_dir}/.env
+# shellcheck source=/dev/null
+source "${script_dir}/.env"
 
 COMMAND=${1:?"command  |DYNAMO-BT  is required as argument \$1"}
 
 # shellcheck disable=SC1073
-JOB_NAME=$(echo "${COMMAND}-dataload-`date +%Y%m%d-%H%M%S`" | tr '[:upper:]' '[:lower:]')
-
+JOB_NAME=$(echo "${COMMAND}-dataload-$(date +%Y%m%d-%H%M%S)" | tr '[:upper:]' '[:lower:]')
 
 function dynamodb_to_bigtable() {
    echo "Loading DynamoDB data from Cloud Storage to Bigtable using job:${JOB_NAME}"
 
-
-gcloud dataflow flex-template run ${JOB_NAME} \
+gcloud dataflow flex-template run "${JOB_NAME}" \
     --template-file-gcs-location "${FLEX_TEMPLATE_SPEC_FILE_PATH}" \
-    --project ${PROJECT_ID}  --region "${REGION}" \
+    --project "${PROJECT_ID}"  --region "${REGION}" \
     --disable-public-ips \
-    --staging-location ${STAGING_LOCATION} \
-    --temp-location ${TEMP_LOCATION} \
+    --staging-location "${STAGING_LOCATION}" \
+    --temp-location "${TEMP_LOCATION}" \
     --parameters inputFilePath="${INPUT_FILEPATH}" \
     --parameters bigtableProjectId="${PROJECT_ID}" \
     --parameters bigtableInstanceId="${BIGTABLE_INSTANCE_ID}" \
@@ -30,15 +29,18 @@ gcloud dataflow flex-template run ${JOB_NAME} \
 
 }
 
-
-
-case $COMMAND in
+case "$COMMAND" in
   "DYNAMO-BT")
     dynamodb_to_bigtable
     ;;
-  *|help)
-    	echo "usage: sh flextemplate-run.sh <command> where command can be one of the following:
-       DYNAMO-BT : Loads data from Cloud Storage to Bigtable
+  help) # Changed this line
+     echo "usage: sh flextemplate-run.sh <command> where command can be one of the following:
+       DYNAMO-BT : Loads data from DynamoDB to Bigtable
+  "
+   ;;
+  *) # Changed this line
+     echo "usage: sh flextemplate-run.sh <command> where command can be one of the following:
+       DYNAMO-BT : Loads data from DynamoDB to Bigtable
   "
    ;;
 esac
