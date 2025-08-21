ba2395f86efc59ac8c0ee54467ec70f7e2c0e0eb
Vannick Trinquier <vannick@google.com>
2025-08-21 11:06:34 +0700

    feat: update tf format to match fast module

--- tools/custom-organization-policy-library/scripts/cvt-tf-constraints.py
+++ tools/custom-organization-policy-library/scripts/cvt-tf-constraints.py
@@ -17,6 +17,19 @@ import os
 import sys
 import yaml
 
+class LiteralString(str):
+    pass
+
+def literal_representer(dumper, data):
+    """
+    This function tells the YAML dumper to use the literal block style ('|')
+    for any string that is an instance of the LiteralString class.
+    The '-' for chomping is handled automatically by ensuring the string
+    has no trailing newline before dumping.
+    """
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+
+yaml.add_representer(LiteralString, literal_representer)
 
 def convert_terraform_factory(doc):
     try:
@@ -31,20 +44,25 @@ def convert_terraform_factory(doc):
 
         if "methodTypes" in doc:
             doc["method_types"] = doc.pop("methodTypes")
+                
+        if "condition" in doc:
+            condition_value = doc["condition"].strip()
+            doc["condition"] = LiteralString(condition_value)
 
         tf_yaml = {}
         _, _, root_key = doc.get("name").rpartition("/")
-        root_prefix_key = root_key # root_key.replace("custom.", "custom.${prefix}")
+        root_prefix_key =  root_key # root_key.replace("custom.", "custom.${prefix}")
         del doc["name"]
         tf_yaml[root_prefix_key] = doc
         return root_key, tf_yaml
 
-    except:
-        print(f"Error converting yaml data:\n{yaml.dump(doc)}")
+    except Exception as e:
+        print(f"Error converting yaml data: {e}\n{yaml.dump(doc)}")
+        return None, None
 
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print(f"Usage: python3 {py_filename} <source_dir> <output_dir>")
         return
 
@@ -57,11 +75,16 @@ def main():
         with open(filepath, "r") as file:
             try:
                 yaml_data = yaml.safe_load(file)
+                if not yaml_data:
+                    continue
+                    
                 root_key, tf_yaml_data = convert_terraform_factory(yaml_data)
-                output_file = f"{output_dir}/{root_key}.yaml"
-                with open(output_file, "w") as outfile:
-                    print(f"[{py_filename}] creating: {output_file}")
-                    yaml.dump(tf_yaml_data, outfile)
+
+                if root_key and tf_yaml_data:
+                    output_file = f"{output_dir}/{root_key}.yaml"
+                    with open(output_file, "w") as outfile:
+                        print(f"[{py_filename}] creating: {output_file}")
+                        yaml.dump(tf_yaml_data, outfile, sort_keys=True)
 
             except yaml.YAMLError as exc:
                 print(f"Error parsing YAML file {filepath}: {exc}")
