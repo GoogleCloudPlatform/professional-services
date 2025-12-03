541ebd270fd6f4976b84b8a1c11e37f3ae6d11f3
Sergey Shcherbakov <sergey.shcherbakov@gmail.com>
2025-12-03 17:13:42 +0100

    Add Cloud Run based example of custom monitoring of CRL distribution endpoints (#1628)
    Co-authored-by: Andrew Gold <41129777+agold-rh@users.noreply.github.com>

---
+++ examples/cloudrun-crl-monitor/crl-monitor/variables.tf
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "target_url" {
+  type = string
+}
+
+variable "schedule" {
+  type    = string
+  default = "* * * * *"
+}
+
+variable "crl_expiration_buffer" {
+  type    = string
+  default = "3600s" # 1 hour default
+}
+
+variable "name" {
+  type = string
+}
+
+variable "alert_autoclose" {
+  type    = string
+  default = "1800s"
+}
